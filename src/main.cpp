9a5490caab2be408d62896207af4de52cc22bfab
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-30 23:48:36 +0200

    UTF-8 as default QString codec (no more fromUtf8)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "newfiledialog.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationVersion(APP_VERSION);
+
+    // Set QString codec to UTF-8
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+#endif
 
     NewFileDialog n;
     n.show();
