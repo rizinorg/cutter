cad2828d1f84988244eb6f448d3e14aa51a58678
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-31 03:45:59 +0200

    add a binary version check to prevent warn about possible binary incompatibility issues

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "newfiledialog.h"
 #include <QApplication>
 #include <QTextCodec>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +15,21 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
 #endif
+
+    // Check r2 version
+    QString r2version = r_core_version();
+    QString localVersion = "" R2_GITTAP;
+    if(r2version != localVersion)
+    {
+        QMessageBox msg;
+        msg.setIcon(QMessageBox::Critical);
+        msg.setWindowIcon(QIcon(":/new/prefix1/img/logo-small.png"));
+        msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+        msg.setWindowTitle("Version mismatch!");
+        msg.setText(QString("The version used to compile iaito (%1) does not match the binary version of radare2 (%2). This could result in unexpected behaviour. Are you sure you want to continue?").arg(localVersion, r2version));
+        if(msg.exec() == QMessageBox::No)
+            return 1;
+    }
 
     NewFileDialog n;
     n.show();
