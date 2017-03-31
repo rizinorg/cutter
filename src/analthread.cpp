df72be09a5f33306746ff296a9f87798f44e6937
ballessay <code@ballessay.de>
2017-04-01 00:43:12 +0200

    cleanup AnalThread
    - removed MainWindow dependency
    - initalized members
    - added custom start function
    - handle thread running on destruction
    - hide QThread::start

--- src/analthread.cpp
+++ src/analthread.cpp
@@ -1,18 +1,33 @@
 #include "analthread.h"
+#include "qrcore.h"
 #include <QDebug>
-#include "mainwindow.h"
 
-AnalThread::AnalThread(MainWindow *w, QWidget *parent) :
-    QThread(parent)
+AnalThread::AnalThread(QWidget *parent) :
+    QThread(parent),
+    core(nullptr),
+    level(2)
 {
-    // Radare core found in:
-    this->w = w;
-    //this->level = 2;
+}
+
+AnalThread::~AnalThread()
+{
+    if (isRunning()) {
+        quit();
+        wait();
+    }
+}
+
+void AnalThread::start(QRCore *core, int level)
+{
+    this->core = core;
+    this->level = level;
+
+    QThread::start();
 }
 
 // run() will be called when a thread starts
 void AnalThread::run()
 {
     //qDebug() << "Anal level: " << this->level;
-    this->w->core->analyze(this->level);
+    core->analyze(this->level);
 }
