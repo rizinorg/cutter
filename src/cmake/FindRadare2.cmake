b82e932ad7de833d1b81490e81cc0d46fa35bb40
C. Balles <code@ballessay.de>
2017-04-03 02:18:41 +0200

    Support sys/user.sh install of radare2

--- src/cmake/FindRadare2.cmake
+++ src/cmake/FindRadare2.cmake
@@ -8,6 +8,7 @@
 find_path(RADARE2_INCLUDE_DIRS
 		NAMES r_core.h r_bin.h r_util.h
 		HINTS
+			"$ENV{HOME}/bin/prefix/radare2/include/libr"
 			/usr/local/include/libr
 			/usr/include/libr)
 
@@ -42,6 +43,7 @@ foreach(libname ${RADARE2_LIBRARY_NAMES})
 	find_library(RADARE2_LIBRARY_${libname}
 			r_${libname}
 			HINTS
+				"$ENV{HOME}/bin/prefix/radare2/lib"
 				/usr/local/lib
 				/usr/lib)
 
