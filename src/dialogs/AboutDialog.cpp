86eaf3a99d013a528ca29d98cad540f6f54bcde5
Florian Märkl <info@florianmaerkl.de>
2017-11-27 18:10:25 +0100

    AboutDialog formatting

--- src/dialogs/AboutDialog.cpp
+++ src/dialogs/AboutDialog.cpp
@@ -15,8 +15,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
                           "<h2>License</h2>"
                           "This Software is released under the GNU General Public License v3.0"
                           "<h2>Authors</h2>"
-                          "xarkes, thestr4ng3r, ballessay"
-                          "Based on Hugo Teso &lt;hugo.teso@gmail.org&gt; work (originally Iaito)."));
+                          "xarkes, thestr4ng3r, ballessay<br/>"
+                          "Based on work by Hugo Teso &lt;hugo.teso@gmail.org&gt; (originally Iaito)."));
 }
 
 AboutDialog::~AboutDialog() {}
