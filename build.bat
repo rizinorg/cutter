09ce852e80d7e3b18ea51870e0c7bf335fb21443
Paul I <>
2018-01-03 17:38:08 +0300

    Cleanup Windows build scripts

--- build.bat
+++ build.bat
@@ -1,43 +1,37 @@
-@echo off
+@ECHO off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo Setting path
-if "%OLDPATH%"=="" set OLDPATH=%PATH%
-if "%QT32PATH%"=="" set QT32PATH=C:\Qt\5.9.2\msvc2015
-if "%QT64PATH%"=="" set QT64PATH=C:\Qt\5.9.2\msvc2015_64
-if "%VSVARSALLPATH%"=="" set VSVARSALLPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
+IF NOT DEFINED QT32PATH SET QT32PATH=C:\Qt\5.9.2\msvc2015
+IF NOT DEFINED QT64PATH SET QT64PATH=C:\Qt\5.9.2\msvc2015_64
+IF NOT DEFINED VSVARSALLPATH SET VSVARSALLPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
 
-if "%1"=="32" (
-    set "PATH=%QT32PATH%\bin;%PATH%"
-    call "%VSVARSALLPATH%" x86
-    set MSBUILDPLATFORM=Win32
-) else if "%1"=="64" (
-    set "PATH=%QT64PATH%\bin;%PATH%"
-    call "%VSVARSALLPATH%" x64
-    set MSBUILDPLATFORM=x64
+if "%1" == "32" (
+    SET "PATH=%QT32PATH%\bin;%PATH%"
+    CALL "%VSVARSALLPATH%" x86
+    SET MSBUILDPLATFORM=Win32
+) else if "%1" == "64" (
+    SET "PATH=%QT64PATH%\bin;%PATH%"
+    CALL "%VSVARSALLPATH%" x64
+    SET MSBUILDPLATFORM=x64
 ) else (
-    echo Usage: build.bat 32/64
-    goto restorepath
+    ECHO Usage: %0 {32^|64}
+    EXIT /B
 )
 
-echo Preparing directory
-rmdir /s /q build%1
-mkdir build%1
-cd build%1
+ECHO Preparing directory
+RMDIR /S /Q build%1
+MKDIR build%1
+CD build%1
 
-echo Building cutter
+ECHO Building cutter
 qmake ..\src\cutter.pro -config release -tp vc
-if not %ERRORLEVEL%==0 exit
+IF !ERRORLEVEL! NEQ 0 EXIT /B
 msbuild /m cutter.vcxproj /p:Configuration=Release;Platform=%MSBUILDPLATFORM%
-if not %ERRORLEVEL%==0 exit
+IF !ERRORLEVEL! NEQ 0 EXIT /B
 
-echo Deploying cutter
-mkdir cutter%1
-move release\cutter.exe cutter%1\cutter.exe
-xcopy /s ..\dist%1 cutter%1\
+ECHO Deploying cutter
+MKDIR cutter%1
+MOVE release\cutter.exe cutter%1\cutter.exe
+XCOPY /S ..\dist%1 cutter%1\
 windeployqt cutter%1\cutter.exe
-cd ..
-
-:restorepath
-echo Restoring path
-set PATH=%OLDPATH%
-set OLDPATH=
+CD ..
