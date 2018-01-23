97ac8c4c41b377d15d708c740146e8cdf3a1a664
xarkes <antide.petit@gmail.com>
2018-01-23 19:12:03 +0100

    Added --shared because fuckit

--- prepare_r2.bat
+++ prepare_r2.bat
@@ -51,7 +51,7 @@ CD radare2
 git clean -xfd
 RMDIR /s /q ..\dist%BITS%
 CALL "%VSVARSALLPATH%" %VARSALL%
-python sys\meson.py --release --prefix="%CD%" --install=..\dist%BITS% --copylib
+python sys\meson.py --release --prefix="%CD%" --install=..\dist%BITS% --shared --copylib
 IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 COPY /Y build\r_userconf.h ..\dist%BITS%\include\libr\
 COPY /Y build\r_version.h ..\dist%BITS%\include\libr\
