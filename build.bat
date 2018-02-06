5a841c18235f0db13088ca7488ef3594e6a76c46
Paul I <pelijah@users.noreply.github.com>
2018-02-07 00:43:06 +0300

    Cleanup Windows build scripts

--- build.bat
+++ build.bat
@@ -5,33 +5,34 @@ IF NOT DEFINED QT32PATH SET QT32PATH=C:\Qt\5.9.2\msvc2015
 IF NOT DEFINED QT64PATH SET QT64PATH=C:\Qt\5.9.2\msvc2015_64
 IF NOT DEFINED VSVARSALLPATH SET VSVARSALLPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
 
-if "%1" == "32" (
+IF "%1" == "32" (
     SET "PATH=%QT32PATH%\bin;%PATH%"
     CALL "%VSVARSALLPATH%" x86
     SET MSBUILDPLATFORM=Win32
-) else if "%1" == "64" (
+) ELSE IF "%1" == "64" (
     SET "PATH=%QT64PATH%\bin;%PATH%"
     CALL "%VSVARSALLPATH%" x64
     SET MSBUILDPLATFORM=x64
-) else (
+) ELSE (
     ECHO Usage: %0 {32^|64}
-    EXIT /B
+    EXIT /B 1
 )
+SET BITS=%1
 
 ECHO Preparing directory
-RMDIR /S /Q build%1
-MKDIR build%1
-CD build%1
+RMDIR /S /Q build%BITS%
+MKDIR build%BITS%
+CD build%BITS%
 
 ECHO Building cutter
 qmake ..\src\cutter.pro -config release -tp vc
-IF !ERRORLEVEL! NEQ 0 EXIT /B
+IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 msbuild /m cutter.vcxproj /p:Configuration=Release;Platform=%MSBUILDPLATFORM%
-IF !ERRORLEVEL! NEQ 0 EXIT /B
+IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 
 ECHO Deploying cutter
-MKDIR cutter%1
-MOVE release\cutter.exe cutter%1\cutter.exe
-XCOPY /S ..\dist%1 cutter%1\
-windeployqt cutter%1\cutter.exe
+MKDIR cutter%BITS%
+MOVE release\cutter.exe cutter%BITS%\cutter.exe
+XCOPY /S ..\dist%BITS% cutter%BITS%\
+windeployqt cutter%BITS%\cutter.exe
 CD ..
--- prepare_r2.bat
+++ prepare_r2.bat
@@ -1,61 +1,45 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET BADARG=1
-FOR %%i IN ("32" "64") DO (IF "%1" == %%i SET BADARG=) 
-IF DEFINED BADARG (
-	ECHO Usage: %0 [32^|64]
-	EXIT /B
-)
-SET BITS=%1
-
 FOR %%i IN (python.exe) DO (IF NOT DEFINED PYTHON SET PYTHON=%%~dp$PATH:i)
 
 IF NOT DEFINED PYTHON SET PYTHON=C:\Program Files\Python36
 IF NOT DEFINED NINJA_URL SET NINJA_URL=https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
 IF NOT DEFINED VSVARSALLPATH SET VSVARSALLPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
 
-IF NOT EXIST %PYTHON%\python.exe EXIT /B
+IF "%1" == "32" (
+    CALL "%VSVARSALLPATH%" x86
+) ELSE IF "%1" == "64" (
+    CALL "%VSVARSALLPATH%" x64
+) ELSE (
+    ECHO Usage: %0 {32^|64}
+    EXIT /B 1
+)
+SET BITS=%1
 
-SET "PYTHONHOME=%PYTHON%"
-SET "PATH=%CD%;%PYTHON%;%PYTHON%\Scripts;%PATH%"
+SET "PATH=%CD%;%PYTHON%;%PATH%"
 
 git submodule update --init
 
 ECHO Downloading meson and ninja
 python -m pip install meson
-IF !ERRORLEVEL! NEQ 0 EXIT /B
+IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 IF NOT EXIST ninja.exe (
 	powershell -Command wget %NINJA_URL% -OutFile ninja.zip && powershell -Command Expand-Archive .\ninja.zip -DestinationPath .\ && DEL ninja.zip
-	IF !ERRORLEVEL! NEQ 0 EXIT /B
+	IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 )
 
-IF NOT "%BITS%" == "32" (
-	SET VARSALL=x64
-	CALL :BUILD
-	IF !ERRORLEVEL! NEQ 0 EXIT /B
-)
-IF NOT "%BITS%" == "64" (
-	SET VARSALL=x86
-	CALL :BUILD
-	IF !ERRORLEVEL! NEQ 0 EXIT /B
-)
-
-ECHO Copying relevant files in cutter_win32
-XCOPY /S /Y dist%BITS%\include\libr cutter_win32\radare2\include\libr\
-EXIT /B
-
-:BUILD
-ECHO Building radare2 (%VARSALL%)
+ECHO Building radare2 (%BITS%)
 CD radare2
 git clean -xfd
-RMDIR /s /q ..\dist%BITS%
-CALL "%VSVARSALLPATH%" %VARSALL%
-python sys\meson.py --release --prefix="%CD%" --install=..\dist%BITS% --shared --copylib
+RMDIR /S /Q ..\dist%BITS%
+python sys\meson.py --release --install=..\dist%BITS% --shared --copylib
 IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 COPY /Y build\r_userconf.h ..\dist%BITS%\include\libr\
 COPY /Y build\r_version.h ..\dist%BITS%\include\libr\
 COPY /Y build\shlr\liblibr2sdb.a ..\dist%BITS%\r_sdb.lib
 CD ..
 COPY /Y dist%BITS%\*.lib cutter_win32\radare2\lib%BITS%\
-EXIT /B 0
+
+ECHO Copying relevant files in cutter_win32
+XCOPY /S /Y dist%BITS%\include\libr cutter_win32\radare2\include\libr\
