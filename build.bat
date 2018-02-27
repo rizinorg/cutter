4b3c709a8c2b92c39554b7b7b55c3555b40879e4
Paul I <pelijah@users.noreply.github.com>
2018-02-27 11:57:53 +0300

    Remove environment settings from *.bat scripts (#342)

--- build.bat
+++ build.bat
@@ -1,23 +1,13 @@
 @ECHO off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-IF NOT DEFINED QT32PATH SET QT32PATH=C:\Qt\5.9.2\msvc2015
-IF NOT DEFINED QT64PATH SET QT64PATH=C:\Qt\5.9.2\msvc2015_64
-IF NOT DEFINED VSVARSALLPATH SET VSVARSALLPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
-
-IF "%1" == "32" (
-    SET "PATH=%QT32PATH%\bin;%PATH%"
-    CALL "%VSVARSALLPATH%" x86
-    SET MSBUILDPLATFORM=Win32
-) ELSE IF "%1" == "64" (
-    SET "PATH=%QT64PATH%\bin;%PATH%"
-    CALL "%VSVARSALLPATH%" x64
+IF "%Platform%" == "X64" (
     SET MSBUILDPLATFORM=x64
+    SET BITS=64
 ) ELSE (
-    ECHO Usage: %0 {32^|64}
-    EXIT /B 1
+    SET MSBUILDPLATFORM=Win32
+    SET BITS=32
 )
-SET BITS=%1
 
 ECHO Preparing directory
 RMDIR /S /Q build%BITS%
@@ -31,8 +21,7 @@ msbuild /m cutter.vcxproj /p:Configuration=Release;Platform=%MSBUILDPLATFORM%
 IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 
 ECHO Deploying cutter
-MKDIR cutter%BITS%
-MOVE release\cutter.exe cutter%BITS%\cutter.exe
-XCOPY /S ..\dist%BITS% cutter%BITS%\
-windeployqt cutter%BITS%\cutter.exe
-CD ..
+MKDIR cutter
+MOVE release\cutter.exe cutter\cutter.exe
+XCOPY /S ..\dist%BITS% cutter\
+windeployqt cutter\cutter.exe
--- prepare_r2.bat
+++ prepare_r2.bat
@@ -1,32 +1,10 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-FOR %%i IN (python.exe) DO (IF NOT DEFINED PYTHON SET PYTHON=%%~dp$PATH:i)
-
-IF NOT DEFINED PYTHON SET PYTHON=C:\Program Files\Python36
-IF NOT DEFINED NINJA_URL SET NINJA_URL=https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
-IF NOT DEFINED VSVARSALLPATH SET VSVARSALLPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
-
-IF "%1" == "32" (
-    CALL "%VSVARSALLPATH%" x86
-) ELSE IF "%1" == "64" (
-    CALL "%VSVARSALLPATH%" x64
+IF "%Platform%" == "X64" (
+    SET BITS=64
 ) ELSE (
-    ECHO Usage: %0 {32^|64}
-    EXIT /B 1
-)
-SET BITS=%1
-
-SET "PATH=%CD%;%PYTHON%;%PATH%"
-
-git submodule update --init
-
-ECHO Downloading meson and ninja
-python -m pip install meson
-IF !ERRORLEVEL! NEQ 0 EXIT /B 1
-IF NOT EXIST ninja.exe (
-	powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget %NINJA_URL% -OutFile ninja.zip" && powershell -Command Expand-Archive .\ninja.zip -DestinationPath .\ && DEL ninja.zip
-	IF !ERRORLEVEL! NEQ 0 EXIT /B 1
+    SET BITS=32
 )
 
 ECHO Building radare2 (%BITS%)
