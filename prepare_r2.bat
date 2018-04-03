c88d8c2a398b48c0c9993c518fb6f78a99af0682
xarkes <antide.petit@gmail.com>
2018-04-03 17:57:29 +0200

    Fixed prepare_r2

--- prepare_r2.bat
+++ prepare_r2.bat
@@ -14,13 +14,13 @@ ECHO Building radare2 (%BITS%)
 CD radare2
 git clean -xfd
 RMDIR /S /Q ..\dist%BITS%
-python sys\meson.py --release --install=..\dist%BITS% --shared --copylib
+python sys\meson.py --release --install=..\dist%BITS% --shared
 IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 COPY /Y build\r_userconf.h ..\dist%BITS%\include\libr\
 COPY /Y build\r_version.h ..\dist%BITS%\include\libr\
 COPY /Y build\shlr\liblibr2sdb.a ..\dist%BITS%\r_sdb.lib
 CD ..
-MOVE /Y dist%BITS%\*.lib cutter_win32\radare2\lib%BITS%\
+MOVE /Y dist%BITS%\lib\* cutter_win32\radare2\lib%BITS%\
 
 ECHO Copying relevant files in cutter_win32
 XCOPY /S /Y dist%BITS%\include\libr cutter_win32\radare2\include\libr\
