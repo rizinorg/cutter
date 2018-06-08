b03862773ffedae55ff21ba1388b97001d14c5eb
mandlebro <casal.filipe@gmail.com>
2018-06-08 11:23:49 +0100

    Added debug toolbar icons

--- src/widgets/DebugToolbar.cpp
+++ src/widgets/DebugToolbar.cpp
@@ -4,36 +4,22 @@
 #include <QAction>
 #include <QPainter>
 
-static QIcon getIconFor(const QString &str)
-{
-    int w = 64;
-    int h = 64;
-
-    QPixmap pixmap(w, h);
-    pixmap.fill(Qt::transparent);
-
-    QPainter pixPaint(&pixmap);
-    pixPaint.setPen(Qt::NoPen);
-    pixPaint.setRenderHint(QPainter::Antialiasing);
-    pixPaint.setBrush(QBrush(QColor(52, 152, 219)));
-    pixPaint.drawEllipse(1, 1, w - 2, h - 2);
-    pixPaint.setPen(Qt::white);
-    pixPaint.setFont(QFont("Verdana", 24, 1));
-    pixPaint.drawText(0, 0, w, h - 2, Qt::AlignCenter, QString(str).toUpper().mid(0, 2));
-    return QIcon(pixmap);
-}
-
 DebugToolbar::DebugToolbar(MainWindow *main, QWidget *parent) :
     QToolBar(parent),
     main(main)
 {
     setObjectName("debugToolbar");
-    QIcon iconPlaceholder = getIconFor("Start");
-    QAction *actionStart = new QAction(getIconFor("Start"), tr("Start"), parent);
-    QAction *actionContinue = new QAction(getIconFor("Continue"), tr("Continue"), parent);
-    QAction *actionContinueUntilMain = new QAction(getIconFor("CU"), tr("Continue until main"), parent);
-    QAction *actionStep = new QAction(getIconFor("Step"), tr("Step"), parent);
-    QAction *actionStepOver = new QAction(getIconFor("SO"), tr("Step over"), parent);
+    QIcon startIcon = QIcon(":/img/icons/play_light.svg");
+    QIcon continueIcon = QIcon(":/img/icons/media-skip-forward_light.svg");
+    QIcon continueUntilIcon = QIcon(":/img/icons/continue_until_main.svg");
+    QIcon stepIcon = QIcon(":/img/icons/step_light.svg");
+    QIcon stepOverIcon = QIcon(":/img/icons/step_over_light.svg");
+
+    QAction *actionStart = new QAction(startIcon, tr("Start debug"), parent);
+    QAction *actionContinue = new QAction(continueIcon, tr("Continue"), parent);
+    QAction *actionContinueUntilMain = new QAction(continueUntilIcon, tr("Continue until main"), parent);
+    QAction *actionStep = new QAction(stepIcon, tr("Step"), parent);
+    QAction *actionStepOver = new QAction(stepOverIcon, tr("Step over"), parent);
     addAction(actionStart);
     addAction(actionContinue);
     addAction(actionContinueUntilMain);
