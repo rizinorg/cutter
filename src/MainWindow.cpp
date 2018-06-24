400a2c184dcfa4628c47b84200882bd1a9422a77
Florian Märkl <info@florianmaerkl.de>
2018-06-24 21:52:54 +0200

    Remove Callgrind Macros

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -342,13 +342,8 @@ void MainWindow::openProject(const QString &project_name)
     finalizeOpen();
 }
 
-#include <valgrind/callgrind.h>
-
 void MainWindow::finalizeOpen()
 {
-    CALLGRIND_ZERO_STATS;
-    CALLGRIND_START_INSTRUMENTATION;
-
     core->getOpcodes();
 
     // Set settings to override any incorrect saved in the project
@@ -365,9 +360,6 @@ void MainWindow::finalizeOpen()
     // Add fortune message
     addOutput("\n" + core->cmd("fo"));
     showMaximized();
-
-    CALLGRIND_DUMP_STATS;
-    CALLGRIND_STOP_INSTRUMENTATION;
 }
 
 bool MainWindow::saveProject(bool quit)
