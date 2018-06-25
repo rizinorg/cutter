6bc291be49529289dbae1a543254c27a300d9182
xarkes <antide.petit@gmail.com>
2018-06-25 09:45:39 +0200

    Test setupInterface

--- src/plugins/pluginSample.py
+++ src/plugins/pluginSample.py
@@ -1,5 +1,6 @@
 import cutter
 from cutter_plugin import CutterPlugin
+from PySide2 import QtCore, QtWidgets
 
 class CutterSamplePlugin(CutterPlugin):
     def setupPlugin(self):
@@ -7,9 +8,17 @@ class CutterSamplePlugin(CutterPlugin):
         self.description = 'A sample plugin written in python.'
         self.version = '1.0'
         self.author = 'xarkes'
+        self.app = QtCore.QCoreApplication.instance()
 
-    def setupInterface(self, main, action):
-        print('TODO')
+    def setupInterface(self):
+        for _ in range(200):
+            print('FUCK', end='')
+        main_window = self.app.findChild('MainWindow')
+        dock_widget = QtWidgets.QDockWidget(main_window)
+        dock_widget.setWindowTitle('Test Widget!!!')
+        for _ in range(200):
+            print(main_window, dock_widget)
+        return dock_widget
 
 
 # Instantiate our plugin
