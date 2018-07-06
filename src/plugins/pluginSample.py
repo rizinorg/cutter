595efe6153de7bc1aa2e62217f45608c1b371492
Florian Märkl <info@florianmaerkl.de>
2018-07-06 20:41:42 +0200

    Return Pointer from setupInterface() in Python Plugin

--- src/plugins/pluginSample.py
+++ src/plugins/pluginSample.py
@@ -1,6 +1,7 @@
 import cutter
 from cutter_plugin import CutterPlugin
 from PySide2 import QtCore, QtWidgets
+import shiboken2
 
 class CutterSamplePlugin(CutterPlugin):
     def setupPlugin(self):
@@ -17,9 +18,12 @@ class CutterSamplePlugin(CutterPlugin):
             if widget.objectName() == "MainWindow":
                 main_window = widget
         dock_widget = QtWidgets.QDockWidget(main_window)
+        dock_widget.setObjectName("FancyDockWidgetFromCoolPlugin")
         dock_widget.setWindowTitle('Test Widget')
         print(main_window, dock_widget)
-        return dock_widget
+        ptr = shiboken2.getCppPointer(dock_widget)[0]
+        print(ptr)
+        return ptr
 
 
 # Instantiate our plugin
