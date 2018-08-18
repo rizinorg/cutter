f92fb71b0ea8390f57a135718e4238876aef4036
Florian Märkl <info@florianmaerkl.de>
2018-08-18 21:28:04 +0200

    Make af from DisassemblyContextMenu more meaningful

--- src/dialogs/RenameDialog.h
+++ src/dialogs/RenameDialog.h
@@ -19,6 +19,8 @@ public:
     void setName(QString fcnName);
     QString getName() const;
 
+    void setPlaceholderText(const QString &text);
+
 private slots:
     void on_buttonBox_accepted();
 
