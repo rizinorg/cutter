72246d5f5f5c6d65178d3565a78211f778446f4b
jamieb122 <5406474+jamieb122@users.noreply.github.com>
2018-09-13 18:29:52 -0700

    Added ability to re-type local vars in Dissassembly Widget.  Can re-type to any loaded structs, types or enums

---
+++ src/dialogs/SetFunctionVarTypes.h
@@ -0,0 +1,36 @@
+#ifndef SETFUNCTIONVARTYPES_H
+#define SETFUNCTIONVARTYPES_H
+
+#include "Cutter.h"
+#include <QDialog>
+
+namespace Ui {
+class SetFunctionVarTypes;
+}
+
+class SetFunctionVarTypes : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SetFunctionVarTypes(QWidget *parent = nullptr);
+    ~SetFunctionVarTypes();
+
+    void setUserMessage(const QString userMessage);
+    void setFcn(RCore* core, RAnalFunction *fcn);
+
+public slots:
+    void on_OkPressed();
+
+
+private:
+    RList                   *fcnVars;
+    RAnalFunction           *fcn;
+    Ui::SetFunctionVarTypes *ui;
+    bool                    validFcn;
+    QStringList          allLoadedTypes;
+    QStringList           getAllTypes();
+
+};
+
+#endif // SETFUNCTIONVARTYPES_H
