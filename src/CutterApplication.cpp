781235ba87d86c810497fffb703cf8d04c48d183
a1ext <a13x4nd3r.t@gmail.com>
2018-10-15 07:11:47 +0300

    Fixed #795 (#829)
    Thanks! Resolves #795

--- src/CutterApplication.cpp
+++ src/CutterApplication.cpp
@@ -177,7 +177,9 @@ void CutterApplication::loadPlugins()
         pluginsDir.cdUp();
     }
 #endif
-    pluginsDir.cd("plugins");
+    if (!pluginsDir.cd("plugins"))
+        return;
+
     foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
         QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
         QObject *plugin = pluginLoader.instance();
