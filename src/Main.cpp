e4a5ccd4132f85afcd82e48920185eabeb99dbc6
Unknown <ilya.kek.lol.orbidol@gmail.com>
2018-10-25 11:56:10 +0000

    wrong QTranslator load directory fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #include "CutterApplication.h"
 #include "MainWindow.h"
 #include <QTranslator>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
@@ -14,12 +15,16 @@ int main(int argc, char *argv[])
     QString language = Config()->getCurrLanguage().toLower();
     auto allLocales = QLocale::matchingLocales(QLocale::AnyLanguage, QLocale::AnyScript, QLocale::AnyCountry);
 
-    for (auto &it : allLocales) {
-        if (it.nativeLanguageName() == language) {
-            QString langPrefix = it.bcp47Name().left(2);
-            t.load(QString(":/translations/cutter_%1.qm").arg(langPrefix));
-            a.installTranslator(&t);
-            break;
+    if (language != "English") {
+        for (auto &it : allLocales) {
+            if (it.nativeLanguageName() == language) {
+                QString langPrefix = it.bcp47Name().left(2);
+                t.load(QString(QCoreApplication::applicationDirPath() + QDir::separator() +
+                               "translations" + QDir::separator() +
+                               "cutter_%1.qm").arg(langPrefix));
+                a.installTranslator(&t);
+                break;
+            }
         }
     }
 
