429469bbf9215dc276d6409666a288950bc7df6a
Paul I <pelijah@users.noreply.github.com>
2018-11-06 14:38:44 +0300

    Download pyside2 wheel via pip directly

--- scripts/bundle_pyside.ps1
+++ scripts/bundle_pyside.ps1
@@ -1,8 +1,5 @@
 $siteDir = $args[0]
-$wheel = "PySide2-5.9.0a1.dev1528453054-5.9.6-cp35.cp36-none-win_amd64.whl"
-$url = "https://download.qt.io/snapshots/ci/pyside/5.9/latest/pyside2/" + $wheel
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget $url -OutFile $wheel
-python -m pip install -I --no-compile -t $siteDir $wheel
+python -m pip install -I --no-compile -t $siteDir --index-url=https://download.qt.io/snapshots/ci/pyside/5.9/latest/ pyside2 --trusted-host download.qt.io
 
 $fileList = @(
 	"__init__.py",
@@ -22,4 +19,3 @@ $fileList = @(
 
 $path = Join-Path -Path $siteDir -ChildPath "PySide2"
 Get-ChildItem -Path $path -Exclude $fileList | Remove-Item -Force -Recurse
-Remove-Item -Force $wheel
