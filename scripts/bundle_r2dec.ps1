87026dfe28e969e39c083eaafeba4c7a11ea593d
Paul I <pelijah@users.noreply.github.com>
2018-11-10 15:10:26 +0300

    Bundle r2dec on Appveyor (#913)
    Thanks :)

---
+++ scripts/bundle_r2dec.ps1
@@ -0,0 +1,13 @@
+$dist = $args[0]
+$python = Split-Path((Get-Command python.exe).Path)
+
+git clone https://github.com/wargio/r2dec-js.git
+cd r2dec-js
+& $python\Scripts\meson.exe --buildtype=release p build
+ninja -C build
+Copy-Item . -Recurse -Destination $dist\radare2\lib\plugins\r2dec-js
+Copy-Item build\core_pdd.dll -Destination $dist\radare2\lib\plugins
+Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\p
+Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\build
+Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\.git
+Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\.github
