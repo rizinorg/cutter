4c6bfad796ed9d82d4eef4108d64fb0573499ce5
a1ext <a13x4nd3r.t@gmail.com>
2018-12-04 00:37:49 +0300

    MAC Build: fixed path bar absence

--- scripts/prepare_package_ui_macos.sh
+++ scripts/prepare_package_ui_macos.sh
@@ -35,7 +35,7 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 925, 525}
+           set the bounds of container window to {400, 100, 925, 508}
            set viewOptions to the icon view options of container window
            set arrangement of viewOptions to not arranged
            set icon size of viewOptions to 122
