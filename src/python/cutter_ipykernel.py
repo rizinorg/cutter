2f335565e8891d609322c7fda054d75639772e4f
Florian Märkl <info@florianmaerkl.de>
2018-12-04 21:38:48 +0100

    Override init_io

--- src/python/cutter_ipykernel.py
+++ src/python/cutter_ipykernel.py
@@ -65,6 +65,32 @@ class CutterIPKernelApp(IPKernelApp):
         # Just skip this. It would only pollute Cutter's output.
         pass
 
+    def init_io(self):
+        """Redirect input streams and set a display hook."""
+        import sys
+        from ipython_genutils.importstring import import_item
+        if self.outstream_class:
+            print("outstream class {}".format(self.outstream_class))
+            outstream_factory = import_item(str(self.outstream_class))
+            sys.stdout.flush()
+
+            e_stdout = None if self.quiet else sys.__stdout__
+            e_stderr = None if self.quiet else sys.__stderr__
+
+            #sys.stdout = outstream_factory(self.session, self.iopub_thread,
+            #                               u'stdout',
+            #                               echo=e_stdout)
+            sys.stderr.flush()
+            #sys.stderr = outstream_factory(self.session, self.iopub_thread,
+            #                               u'stderr',
+            #                               echo=e_stderr)
+        if self.displayhook_class:
+            displayhook_factory = import_item(str(self.displayhook_class))
+            self.displayhook = displayhook_factory(self.session, self.iopub_socket)
+            sys.displayhook = self.displayhook
+
+        self.patch_io()
+
 
 def launch_ipykernel(argv):
     app = CutterIPKernelApp.instance()
@@ -72,6 +98,7 @@ def launch_ipykernel(argv):
     def run_kernel():
         import asyncio
         asyncio.set_event_loop(asyncio.new_event_loop())
+        print("We are on thread {}, NOT SOME DUMMY SHIT!!!!".format(threading.current_thread().name))
         app.kernel_class = CutterIPythonKernel
         # app.log_level = logging.DEBUG
         app.initialize(argv[3:])
