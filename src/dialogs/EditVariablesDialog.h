99b1fde47dcb36c8886659add0b15237f46418d9
Florian Märkl <info@florianmaerkl.de>
2018-12-19 09:39:23 +0100

    Cleanup EditVariablesDialog (#1007)
    Originally SetFunctionVarTypes

---
+++ src/dialogs/EditVariablesDialog.h
@@ -0,0 +1,30 @@
+#ifndef EDITVARIABLESDIALOG_H
+#define EDITVARIABLESDIALOG_H
+
+#include "Cutter.h"
+#include <QDialog>
+
+namespace Ui {
+class EditVariablesDialog;
+}
+
+class EditVariablesDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit EditVariablesDialog(RVA offset, QWidget *parent = nullptr);
+    ~EditVariablesDialog();
+
+private slots:
+    void applyFields();
+    void updateFields();
+
+private:
+    Ui::EditVariablesDialog *ui;
+    QList<VariableDescription> variables;
+
+    void populateTypesComboBox();
+};
+
+#endif // EDITVARIABLESDIALOG_H
