783685dc16ef0e8a703cb43704573e65725a2367
Paul I <pelijah@users.noreply.github.com>
2019-01-05 13:16:47 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -17,7 +17,7 @@ RMDIR /S /Q %BUILDDIR%
 MKDIR %BUILDDIR%
 
 ECHO Prepare translations
-lrelease.exe .\src\cutter.pro
+FOR %%i in (src\translations\*.ts) DO lrelease %%i
 
 RMDIR /S /Q %BUILDDIR%\translations
 MKDIR %BUILDDIR%\translations
