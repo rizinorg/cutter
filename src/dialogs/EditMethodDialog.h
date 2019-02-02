1ee1d7d948a46fc48ced9f98e92a7ed49e56103e
Florian Märkl <info@florianmaerkl.de>
2019-02-02 14:14:39 +0100

    Fix Seek to VTable and add some docs

--- src/dialogs/EditMethodDialog.h
+++ src/dialogs/EditMethodDialog.h
@@ -17,6 +17,9 @@ class EditMethodDialog : public QDialog
     Q_OBJECT
 
 public:
+    /*!
+     * \param classFixed whether the user should be able to change the class. If false, a QComboBox will be shown, otherwise a plain QLabel.
+     */
     explicit EditMethodDialog(bool classFixed, QWidget *parent = nullptr);
     ~EditMethodDialog();
 
@@ -26,8 +29,25 @@ public:
     QString getClass();
     AnalMethodDescription getMethod();
 
-    static bool showDialog(const QString &title, QString *className, AnalMethodDescription *desc, QWidget *parent = nullptr);
+    /*!
+     * \brief Helper function to display the dialog
+     *
+     * \param title title of the dialog
+     * \param classFixed whether the user should be able to change the class
+     * \param className initial class name, will be overwritten if the user changed the class
+     * \param desc initial data for the method information
+     * \return whether the dialog was accepted by the user
+     */
+    static bool showDialog(const QString &title, bool classFixed, QString *className, AnalMethodDescription *desc, QWidget *parent = nullptr);
+
+    /*!
+     * \brief Show the dialog to add a new method a given class
+     */
     static void newMethod(QString className = nullptr, const QString &meth = QString(), QWidget *parent = nullptr);
+
+    /*!
+     * \brief Show the dialog to edit a given method of a given class
+     */
     static void editMethod(const QString &className, const QString &meth, QWidget *parent = nullptr);
 
 private slots:
@@ -42,6 +62,9 @@ private:
 
     QComboBox *classComboBox = nullptr;
     QLabel *classLabel = nullptr;
+    /*!
+     * This will only be used when the dialog was created with classFixed = true in order to remember the class name.
+     */
     QString fixedClass;
 
     bool inputValid();
