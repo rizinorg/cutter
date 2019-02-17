0144823e58feb78801eeba586208fb4847fc9560
Florian Märkl <info@florianmaerkl.de>
2019-02-17 14:51:31 +0100

    Build Python Bindings with meson

--- src/bindings/src_list.py
+++ src/bindings/src_list.py
@@ -8,7 +8,7 @@ import sys
 script_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_cpp_files_gen(args):
+def get_cpp_files_gen(args, include_package=True):
     ts_tree = et.parse(os.path.join(script_path, "bindings.xml"))
     ts_root = ts_tree.getroot()
 
@@ -20,10 +20,13 @@ def get_cpp_files_gen(args):
     cpp_files_gen = [f"{package.lower()}_module_wrapper.cpp"]
     cpp_files_gen.extend([f"{typename.lower()}_wrapper.cpp" for typename in types])
 
+    if include_package:
+        cpp_files_gen = [os.path.join(package, f) for f in cpp_files_gen]
+
     if len(args) > 0:
-        return [os.path.join(args[0], package, f) for f in cpp_files_gen]
-    else:
-        return [os.path.join(package, f) for f in cpp_files_gen]
+        cpp_files_gen = [os.path.join(args[0], f) for f in cpp_files_gen]
+
+    return cpp_files_gen
 
 
 def cmd_cmake(args):
@@ -34,7 +37,11 @@ def cmd_qmake(args):
     sys.stdout.write("\n".join(get_cpp_files_gen(args)) + "\n")
 
 
-cmds = {"cmake": cmd_cmake, "qmake": cmd_qmake}
+def cmd_meson(args):
+    sys.stdout.write(";".join(get_cpp_files_gen(args, include_package=False)))
+
+
+cmds = {"cmake": cmd_cmake, "qmake": cmd_qmake, "meson": cmd_meson}
 
 if len(sys.argv) < 2 or sys.argv[1] not in cmds:
     print(f"""usage: {sys.argv[0]} [{"/".join(cmds.keys())}] [base path]""")
