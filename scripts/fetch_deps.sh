b3854839723a5e124e0c9046a90aaf8fa299864b
Florian Märkl <info@florianmaerkl.de>
2019-02-24 16:31:29 +0100

    Update cutter-deps

--- scripts/fetch_deps.sh
+++ scripts/fetch_deps.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd $(dirname "${BASH_SOURCE[0]}")/..
+mkdir -p cutter-deps && cd cutter-deps
+
 FILE=cutter-deps.tar.gz
-MD5=6b25169d8a4228df0516892875934166
-URL=https://github.com/thestr4ng3r/cutter-deps/releases/download/untagged-53327f1ed48017700884/cutter-deps.tar.gz
+MD5=e150049c5587fbaa581fa7e7266f3f3c
+URL=https://github.com/thestr4ng3r/cutter-deps/releases/download/untagged-8ab99b98f962294e705b/cutter-deps.tar.gz
 
 curl -L "$URL" -o "$FILE" || exit 1
 echo "$MD5 $FILE" | md5sum -c - || exit 1
