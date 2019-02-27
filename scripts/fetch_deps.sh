3016580ff761cf81c8cbb8b0aa811a3af2a82ebf
Florian Märkl <info@florianmaerkl.de>
2019-02-27 11:59:40 +0100

    Add update_deps.py and update

--- scripts/fetch_deps.sh
+++ scripts/fetch_deps.sh
@@ -4,8 +4,8 @@ cd $(dirname "${BASH_SOURCE[0]}")/..
 mkdir -p cutter-deps && cd cutter-deps
 
 FILE=cutter-deps.tar.gz
-MD5=75f6321311266b5326de1b9e8cb65ef2
-URL=https://github.com/thestr4ng3r/cutter-deps/releases/download/untagged-e4ad7bf5c4c756f4862d/cutter-deps.tar.gz
+MD5=0805fa6a1626ce787f952b300e2b321d
+URL=https://github.com/radareorg/cutter-deps/releases/download/v2/cutter-deps.tar.gz
 
 curl -L "$URL" -o "$FILE" || exit 1
 echo "$MD5 $FILE" | md5sum -c - || exit 1
