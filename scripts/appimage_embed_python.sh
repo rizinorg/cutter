1efc9cc8a24511e3416ed45eed9e430a19a7bcce
Florian Märkl <info@florianmaerkl.de>
2019-02-28 13:44:56 +0100

    Fix appimage_embed_python.sh

--- scripts/appimage_embed_python.sh
+++ scripts/appimage_embed_python.sh
@@ -12,9 +12,10 @@ appdir=$1
 
 echo "Embedding Python from prefix $python_prefix in appdir $appdir"
 
-cp -RT "$python_prefix" "$appdir/usr/" || exit 1
+mkdir -p "$appdir/usr"
 cd "$appdir/usr/" || exit 1
 
+cp -RT "$python_prefix" "." || exit 1
 echo "Cleaning up embedded Python"
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 rm -r lib/$python_version/test lib/$python_version/idlelib lib/$python_version/curses lib/$python_version/lib2to3
@@ -32,5 +33,5 @@ echo "PySide is at $pyside_prefix"
 if [ "$pyside_prefix" == "$python_prefix" ]; then
 	echo "Prefixes are equal, not copying anything from lib"
 else
-	cp -RT "$python_prefix/lib/$python_version" "$appdir/usr/lib/$python_version" || exit 1
+	cp -RT "$pyside_prefix/lib/$python_version" "lib/$python_version" || exit 1
 fi
