b48eb9eee793a5e75bf9088bc69c0dab8bdf233b
Unknown <ilya.kek.lol.orbidol@gmail.com>
2019-03-01 17:43:21 +0300

    debug

--- scripts/prepare_breakpad.bat
+++ scripts/prepare_breakpad.bat
@@ -1,9 +1,11 @@
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget https://storage.googleapis.com/chrome-infra/depot_tools.zip -OutFile depot_tools.zip "
 7z -bd x %CD%\depot_tools.zip -odepot_tools
-depot_tools\update_depot_tools
+%CD%\depot_tools\update_depot_tools
+ECHO KEK
 set "PATH=%CD%\depot_tools;%PATH%"
-  
+DIR
 mkdir %CD%\breakpad
+DIR
 cd %CD%\breakpad
 ECHO klsejgseklgjseklgjseklgjseklgjseklgjsel
 DIR
