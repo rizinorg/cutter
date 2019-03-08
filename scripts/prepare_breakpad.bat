8210da1756b9404bc5de7b2df6ba47d19d7b4722
Unknown <ilya.kek.lol.orbidol@gmail.com>
2019-03-08 20:46:42 +0300

    debug

--- scripts/prepare_breakpad.bat
+++ scripts/prepare_breakpad.bat
@@ -1,19 +1,22 @@
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget https://storage.googleapis.com/chrome-infra/depot_tools.zip -OutFile depot_tools.zip "
 7z -bd x %CD%\depot_tools.zip -odepot_tools
-%CD%\depot_tools\update_depot_tools
-@ECHO ON
-ECHO KEK
-set "PATH=%CD%\depot_tools;%PATH%"
-DIR
+depot_tools\update_depot_tools
+SET "BUFF_PATH=%PATH%"
+SET "PATH=%CD%\depot_tools;%PATH%"
 mkdir %CD%\breakpad
-DIR
-cd %CD%\breakpad
-ECHO klsejgseklgjseklgjseklgjseklgjseklgjsel
-DIR
+SET "DAT_DIR=%CD%"
+CD %CD%\breakpad
 fetch breakpad
 gclient sync
-ECHO klsejgseklgjseklgjseklgjseklgjseklgjsel 22
-DIR
-cd ..
-
-msbuild /m %CD%\breakpad\src\src\client\windows\handler\exception_handler.vcxproj /p:Configuration=release
+CD %DAT_DIR%
+DEL %DAT_DIR%\breakpad\src\src\client\windows\breakpad_client.gyp
+DEL %DAT_DIR%\breakpad\src\src\client\windows\breakpad_client.sln
+DEL %DAT_DIR%\breakpad\src\src\client\windows\common.vcxproj
+DEL %DAT_DIR%\breakpad\src\src\client\windows\common.vcxproj.filters
+DEL %DAT_DIR%\breakpad\src\src\client\windows\build_all.vcxproj
+COPY %CD%\scripts\breakpad_client.gyp %CD%\breakpad\src\src\client\windows
+CD %CD%\breakpad\src\src
+tools\gyp\gyp.bat --no-circular-check client\windows\breakpad_client.gyp -Dwin_release_RuntimeLibrary=2 -Dwin_debug_RuntimeLibrary=2  -Dplatform=%ARCH% -Dconfiguration=release
+SET "PATH=%BUFF_PATH%"
+msbuild /m %CD%\client\windows\breakpad_client.sln /p:Configuration=release /p:Platform=%ARCH%
+CD %DAT_DIR%
