5e8ab48990808d11ed5ab1e5dd21714167c71a2b
ITAYC0HEN <itaycohen23@gmail.com>
2019-03-08 12:11:10 +0200

    improve qframe

--- src/widgets/FunctionsWidget.cpp
+++ src/widgets/FunctionsWidget.cpp
@@ -710,7 +710,9 @@ void FunctionsWidget::setScrollMode()
 
 void FunctionsWidget::setTooltipStylesheet()
 {
-    setStyleSheet(QString("QToolTip { border: 2px rgb(191, 194, 197); max-width: %1px; opacity: 230; background-color: %2; padding: 10px; color: %3; }")
+    setStyleSheet(QString("QToolTip { border-width: 1px; max-width: %1px;" \
+                          "opacity: 230; background-color: %2;" \
+                          "color: %3; border-color: %3;}")
                   .arg(kMaxTooltipWidth)
                   .arg(Config()->getColor("gui.tooltip.background").name())
                   .arg(Config()->getColor("gui.tooltip.foreground").name()));
