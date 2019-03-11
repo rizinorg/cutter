5e94832a3c2910ea4975924a0af6e9163b280c2a
Unknown <ilya.kek.lol.orbidol@gmail.com>
2019-03-11 23:33:44 +0300

    do not create symbols

--- scripts/prepare_breakpad.bat
+++ scripts/prepare_breakpad.bat
@@ -21,18 +21,18 @@ DEL %CD%\common.vcxproj.filters
 DEL %CD%\build_all.vcxproj
 COPY %ROOT_DIR%\scripts\breakpad_client.gyp %CD%
 
-CD %ROOT_DIR%\breakpad\src\src\tools\windows\dump_syms
-DEL %CD%\dump_syms.gyp
-DEL %CD%\dump_syms.vcxproj
-DEL %CD%\dump_syms.vcproj
-DEL %CD%\dump_syms.sln
-COPY %ROOT_DIR%\scripts\dump_syms.gyp %CD%
+rem  CD %ROOT_DIR%\breakpad\src\src\tools\windows\dump_syms
+rem  DEL %CD%\dump_syms.gyp
+rem  DEL %CD%\dump_syms.vcxproj
+rem  DEL %CD%\dump_syms.vcproj
+rem  DEL %CD%\dump_syms.sln
+rem  COPY %ROOT_DIR%\scripts\dump_syms.gyp %CD%
 
 CD %ROOT_DIR%\breakpad\src\src
 powershell -Command "tools\gyp\gyp.bat --no-circular-check client\windows\breakpad_client.gyp -Dwin_release_RuntimeLibrary=2 -Dwin_debug_RuntimeLibrary=2  -Dplatform=%ARCH% -Dconfiguration=release"
-powershell -Command "tools\gyp\gyp.bat --no-circular-check tools\windows\dump_syms\dump_syms.gyp -Dwin_release_RuntimeLibrary=2 -Dwin_debug_RuntimeLibrary=2  -Dplatform=%ARCH% -Dconfiguration=release"
+rem  powershell -Command "tools\gyp\gyp.bat --no-circular-check tools\windows\dump_syms\dump_syms.gyp -Dwin_release_RuntimeLibrary=2 -Dwin_debug_RuntimeLibrary=2  -Dplatform=%ARCH% -Dconfiguration=release"
 
 set PATH=%BUFF_PATH%
 msbuild /m %CD%\client\windows\breakpad_client.sln /p:Configuration=release /p:Platform=%ARCH%
-msbuild /m %CD%\tools\windows\dump_syms\dump_syms.sln /p:Configuration=release /p:Platform=%ARCH%
+rem  msbuild /m %CD%\tools\windows\dump_syms\dump_syms.sln /p:Configuration=release /p:Platform=%ARCH%
 CD %ROOT_DIR%
