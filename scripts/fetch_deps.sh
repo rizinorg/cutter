a73c9e77743812126ef88df82f0f1c8f3903de4b
Florian Märkl <info@florianmaerkl.de>
2019-03-13 20:00:04 +0100

    Fetch deps for Windows

--- scripts/fetch_deps.sh
+++ scripts/fetch_deps.sh
@@ -4,25 +4,35 @@ cd $(dirname "${BASH_SOURCE[0]}")/..
 mkdir -p cutter-deps && cd cutter-deps
 
 LINUX_FILE="cutter-deps-linux.tar.gz"
-LINUX_MD5=b52ab4f06cb9e4b92372022c37920bf1
-LINUX_URL=https://github.com/radareorg/cutter-deps/releases/download/v6/cutter-deps-linux.tar.gz
+LINUX_MD5=d135cfd37b67b9d1813171479f571860
+LINUX_URL=https://github.com/radareorg/cutter-deps/releases/download/v7/cutter-deps-linux.tar.gz
 
 MACOS_FILE="cutter-deps-macos.tar.gz"
-MACOS_MD5=85b663ea73b80663a532dfafe998905d
-MACOS_URL=https://github.com/radareorg/cutter-deps/releases/download/v6/cutter-deps-macos.tar.gz
+MACOS_MD5=15027864b2e0f0bc0c7ec796da89d23b
+MACOS_URL=https://github.com/radareorg/cutter-deps/releases/download/v7/cutter-deps-macos.tar.gz
 
-UNAME_S="$(uname -s)"
-if [ "$UNAME_S" == "Linux" ]; then
-	FILE="${LINUX_FILE}"
-	MD5="${LINUX_MD5}"
-	URL="${LINUX_URL}"
-elif [ "$UNAME_S" == "Darwin" ]; then
-	FILE="${MACOS_FILE}"
-	MD5="${MACOS_MD5}"
-	URL="${MACOS_URL}"
+WIN_FILE="cutter-deps-win.tar.gz"
+WIN_MD5=c6fa37ac61e42239402dbc1d20f28607
+WIN_URL=https://github.com/radareorg/cutter-deps/releases/download/v7/cutter-deps-win.tar.gz
+
+if [ "$OS" == "Windows_NT" ]; then
+	FILE="${WIN_FILE}"
+	MD5="${WIN_MD5}"
+	URL="${WIN_URL}"
 else
-	echo "Unsupported Platform: $UNAME_S"
-	exit 1
+	UNAME_S="$(uname -s)"
+	if [ "$UNAME_S" == "Linux" ]; then
+		FILE="${LINUX_FILE}"
+		MD5="${LINUX_MD5}"
+		URL="${LINUX_URL}"
+	elif [ "$UNAME_S" == "Darwin" ]; then
+		FILE="${MACOS_FILE}"
+		MD5="${MACOS_MD5}"
+		URL="${MACOS_URL}"
+	else
+		echo "Unsupported Platform: uname -s => $UNAME_S, \$OS => $OS"
+		exit 1
+	fi
 fi
 
 curl -L "$URL" -o "$FILE" || exit 1
@@ -39,5 +49,8 @@ else
 fi
 
 tar -xf "$FILE" || exit 1
-./relocate.sh || exit 1
+
+if [ -f relocate.sh ]; then
+	./relocate.sh || exit 1
+fi
 
