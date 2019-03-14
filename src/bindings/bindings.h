0bbee6f9036a4187593b9090365a7b0f59e4de74
Florian Märkl <info@florianmaerkl.de>
2019-03-14 21:02:26 +0100

    same thing

--- src/bindings/bindings.h
+++ src/bindings/bindings.h
@@ -2,6 +2,10 @@
 #ifndef CUTTER_BINDINGS_H
 #define CUTTER_BINDINGS_H
 
+#ifdef _WIN32
+#include <winsock2.h>
+#endif
+
 #include "../core/Cutter.h"
 #include "../common/Configuration.h"
 #include "../core/MainWindow.h"
