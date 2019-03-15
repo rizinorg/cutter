de5da419bb53240c359f5307f0d31e926cadaf50
Unknown <ilya.kek.lol.orbidol@gmail.com>
2019-03-15 17:07:29 +0300

    debug

--- scripts/prepare_breakpad_linux.sh
+++ scripts/prepare_breakpad_linux.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/google/breakpad.git
 cd breakpad
 git clone https://chromium.googlesource.com/linux-syscall-support src/third_party/lss
-./configure --prefix=`pwd`/prefix && make && make install
+CFLAGS=-w CXXFLAGS=-w ./configure --prefix=`pwd`/prefix && make && make install
 
 export CUSTOM_BREAKPAD_PREFIX="`pwd`/prefix"
 cd ..
