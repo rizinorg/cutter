d48da93ee8d6ad02bdd2002d0473d61b643ad2f6
Florian Märkl <info@florianmaerkl.de>
2019-03-16 22:48:50 +0100

    Update deploy_docs.sh

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -2,9 +2,16 @@
 
 cd $(dirname "${BASH_SOURCE[0]}")/..
 
+echo "Cloning current cutter.re"
+
 git clone git@github.com:radareorg/cutter.re.git || exit 1
+
+echo "Updating docs"
+
 rm -rf cutter.re/docs
 cp -a docs/build/html cutter.re/docs || exit 1
+
+echo "Committing new changes"
 
 cd cutter.re || exit 1
 git add . || exit 1
