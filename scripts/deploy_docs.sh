08f2ef10cc337e7f473dd905bbd2960ad0c543d7
Florian Märkl <info@florianmaerkl.de>
2019-03-16 23:50:25 +0100

    Append original commit info

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -3,10 +3,11 @@
 cd $(dirname "${BASH_SOURCE[0]}")/..
 
 cutter_timestamp=$(git show -s --format=%ct)
+cutter_commit="$(git show -s --format="%H %s")"
 
 echo "Cloning current cutter.re"
 
-git clone git@github.com:radareorg/cutter.re.git || exit 1
+git clone --depth 1 git@github.com:radareorg/cutter.re.git || exit 1
 
 echo "Updating docs"
 
@@ -24,6 +25,6 @@ fi
 
 git add . || exit 1
 git diff --cached --quiet && echo "No changes." && exit 0
-git commit -m "Update docs from radareorg/cutter"
+printf "Update docs from radareorg/cutter\n\nOriginal Commit:\n$cutter_commit" | git commit -F -
 git push origin master || exit 1
 
