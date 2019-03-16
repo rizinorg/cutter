7d8299ebd6387ad1d7e50489d0f354ca35b65598
vane11ope <vane11opeschw33tz@gmail.com>
2019-03-16 23:48:58 +0900

    Ugly hack just to avoid mess with the Extra Widgets

--- src/widgets/GraphWidget.cpp
+++ src/widgets/GraphWidget.cpp
@@ -6,7 +6,17 @@
 GraphWidget::GraphWidget(MainWindow *main, QAction *action) :
     CutterDockWidget(main, action)
 {
-    setObjectName("Graph");
+    /*
+     * Ugly hack just for the layout issue
+     * QSettings saves the state with the object names
+     * By doing this hack,
+     * you can at least avoid some mess by dismissing all the Extra Widgets
+     */
+    QString name = "Graph";
+    if (!action) {
+        name = "Extra Graph";
+    }
+    setObjectName(name);
     setAllowedAreas(Qt::AllDockWidgetAreas);
     graphView = new DisassemblerGraphView(this);
     setWidget(graphView);
