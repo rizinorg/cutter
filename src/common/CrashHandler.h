1346c27df800fdcac583eadea912f1c8b815a093
Unknown <ilya.kek.lol.orbidol@gmail.com>
2019-03-17 14:03:50 +0300

    add docs

--- src/common/CrashHandler.h
+++ src/common/CrashHandler.h
@@ -5,7 +5,7 @@
  * @fn void initCrashHandler()
  *
  * If CUTTER_ENABLE_CRASH_REPORTS is true, initializes
- * crash handling and reporting, otherwise do nothing.
+ * crash handling and reporting, otherwise does nothing.
 */
 void initCrashHandler();
 
