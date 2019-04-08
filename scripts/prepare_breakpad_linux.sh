ed637e48e31b96ab032bb2e5850218fce9b52d65
Florian Märkl <info@florianmaerkl.de>
2019-04-08 20:10:25 +0200

    Copy Breakpad.framework into Bundle

--- scripts/prepare_breakpad_linux.sh
+++ scripts/prepare_breakpad_linux.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd src
 git clone https://github.com/google/breakpad.git
 cd breakpad
 git clone https://chromium.googlesource.com/linux-syscall-support src/third_party/lss
-CFLAGS=-w CXXFLAGS=-w ./configure --prefix=`pwd`/prefix && make && make install
+CFLAGS=-w CXXFLAGS=-w ./configure --prefix=`pwd`/prefix && make -j4 && make install || exit 1
 
 export CUSTOM_BREAKPAD_PREFIX="`pwd`/prefix"
-cd ../..
+cd ..
