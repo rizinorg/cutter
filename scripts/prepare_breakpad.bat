e7d09e708b11b3c3051a8a0e04a696962032b889
Unknown <ilya.kek.lol.orbidol@gmail.com>
2019-06-17 20:25:20 +0300

    init commit

--- scripts/prepare_breakpad.bat
+++ scripts/prepare_breakpad.bat
@@ -12,6 +12,7 @@ mkdir %ROOT_DIR%\src\breakpad
 CD %ROOT_DIR%\src\breakpad
 powershell -Command "fetch breakpad"
 powershell -Command "gclient sync"
+powershell -Command "git reset --hard 756daa536ad819eff80172aaab262fb71d1e89fd"
 
 CD %ROOT_DIR%\src\breakpad\src\src\client\windows
 DEL %CD%\breakpad_client.gyp
