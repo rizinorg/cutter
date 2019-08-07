dd168b19110483cedc8884516097bfeaa08029e4
xarkes <antide.petit@gmail.com>
2019-08-07 22:16:41 +0200

    Push documentation at the proper place

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -12,8 +12,7 @@ git clone --depth 1 git@github.com:radareorg/cutter.re.git || exit 1
 echo "Updating docs"
 
 rm -rf cutter.re/docs
-rm -rf cutter.re/_site/docs
-cp -a docs/build/html cutter.re/_site/docs || exit 1
+cp -a docs/build/html cutter.re/docs || exit 1
 
 echo "Committing new changes"
 
