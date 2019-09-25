91c6142ab8075320d594d2f91271538e890b4935
Paul I <pelijah@users.noreply.github.com>
2019-09-25 16:59:23 +0300

    Update bundle_r2dec.ps1 (#1789)

--- scripts/bundle_r2dec.ps1
+++ scripts/bundle_r2dec.ps1
@@ -3,11 +3,6 @@ $python = Split-Path((Get-Command python.exe).Path)
 
 git clone https://github.com/wargio/r2dec-js.git
 cd r2dec-js
-& $python\Scripts\meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS p build
-ninja -C build
-Copy-Item . -Recurse -Destination $dist\radare2\lib\plugins\r2dec-js
-Copy-Item build\core_pdd.dll -Destination $dist\radare2\lib\plugins
-Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\p
-Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\build
-Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\.git
-Remove-Item -Recurse -Force $dist\radare2\lib\plugins\r2dec-js\.github
+& $python\Scripts\meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS --prefix=$dist\radare2 --libdir=lib\plugins --datadir=lib\plugins p build
+ninja -C build install
+Remove-Item -Recurse -Force $dist\radare2\lib\plugins\core_pdd.lib
