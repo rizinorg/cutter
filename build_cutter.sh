8954eee64a7e38d6d1482363ad8542b99ce2d2f3
0x24d <daniel.longfield@tuta.io>
2019-09-30 21:16:55 +0100

    Make further small changes to Dockerfile to improve efficiency, also add .dockerignore to only copy required files

--- build_cutter.sh
+++ build_cutter.sh
@@ -10,18 +10,6 @@ BUILD="$(pwd)/build"
 QMAKE_CONF=$*
 ROOT_DIR=$(pwd)
 
-check_r2() {
-	r2 -v >/dev/null 2>&1
-	if [ $? = 0 ]; then
-		R2COMMIT=$(r2 -v | tail -n1 | sed "s,commit: \\(.*\\) build.*,\\1,")
-		SUBMODULE=$(git submodule | grep "radare2" | awk '{print $1}')
-		if [ "$R2COMMIT" = "$SUBMODULE" ]; then
-			return 0
-		fi
-	fi
-	return 1
-}
-
 find_qmake() {
 	qmakepath=$(command -v qmake-qt5)
 	if [ -z "$qmakepath" ]; then
@@ -77,25 +65,6 @@ prepare_breakpad() {
 		. $ROOT_DIR/scripts/prepare_breakpad_macos.sh
 	fi
 }
-
-# Build radare2
-#check_r2
-#if [ $? -eq 1 ]; then
-#    printf "A (new?) version of radare2 will be installed. Do you agree? [Y/n] "
-#    read -r answer
-#    if [ -z "$answer" ] || [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
-#        R2PREFIX=${1:-"/usr"}
-#        git submodule init && git submodule update
-#        cd radare2 || exit 1
-#        ./sys/install.sh "$R2PREFIX"
-#        cd ..
-#    else
-#        echo "Sorry but this script won't work otherwise. Read the README."
-#        exit 1
-#    fi
-#else
-#    echo "Correct radare2 version found, skipping..."
-#fi
 
 # Create translations
 $(find_lrelease) ./src/Cutter.pro
