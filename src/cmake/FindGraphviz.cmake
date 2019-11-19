7384fd4af8445438af8fce3d386512f8982d38bd
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2019-11-19 23:11:44 +0200

    Improve compatibility with cmake 3.10.

--- src/cmake/FindGraphviz.cmake
+++ src/cmake/FindGraphviz.cmake
@@ -1,8 +1,10 @@
 set (_module Graphviz)
 
 find_package(PkgConfig REQUIRED)
-if (NOT (CMAKE_VERSION VERSION_LESS "3.6.0"))
+if (NOT (CMAKE_VERSION VERSION_LESS "3.13.0"))
     pkg_check_modules(GVC IMPORTED_TARGET GLOBAL libgvc)
+elseif (NOT (CMAKE_VERSION VERSION_LESS "3.11.0"))
+    pkg_check_modules(GVC IMPORTED_TARGET libgvc)
 else()
     pkg_check_modules(GVC libgvc)
 endif()
@@ -13,7 +15,7 @@ find_package_handle_standard_args(${_module}
         REQUIRED_VARS GVC_INCLUDE_DIRS)
 
 if (${GVC_FOUND})
-    if (CMAKE_VERSION VERSION_LESS "3.6.0")
+    if (CMAKE_VERSION VERSION_LESS "3.11.0")
         add_library(${_module}::GVC INTERFACE IMPORTED)
         set_target_properties(${_module}::GVC PROPERTIES
                 INTERFACE_INCLUDE_DIRECTORIES "${GVC_INCLUDE_DIRS}")
