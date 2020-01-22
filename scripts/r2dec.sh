b3bbda5c47a012874aced4244c93921de18c160c
Florian Märkl <info@florianmaerkl.de>
2020-01-22 13:36:26 +0100

    Use set -e in r2dec.sh

--- scripts/r2dec.sh
+++ scripts/r2dec.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -e
+
 SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 cd "$SCRIPTPATH/.."
 
 if [[ ! -d r2dec-js ]]; then
-	git clone --depth 1 https://github.com/wargio/r2dec-js.git || exit 1
+	git clone --depth 1 https://github.com/wargio/r2dec-js.git
 fi
 
-cd r2dec-js || exit 1
-rm -rf build || exit 1
-mkdir build && cd build || exit 1
-meson --buildtype=release --libdir=share/radare2/plugins --datadir=share/radare2/plugins "$@" ../p || exit 1
-ninja || exit 1
-ninja install || exit 1
+cd r2dec-js
+rm -rf build
+mkdir build && cd build
+meson --buildtype=release --libdir=share/radare2/plugins --datadir=share/radare2/plugins "$@" ../p
+ninja
+ninja install
 
