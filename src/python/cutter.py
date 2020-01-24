47e92a373f6b7567b9a3790a3600ea127bbbdacd
Florian Märkl <info@florianmaerkl.de>
2020-01-24 15:23:59 +0100

    Expose CutterApplication to Python and add accessor

--- src/python/cutter.py
+++ src/python/cutter.py
@@ -6,6 +6,14 @@ try:
 
     def core():
         return CutterCore.instance()
+
+    def app():
+        # see https://forums.autodesk.com/t5/3ds-max-programming/qapplication-instance-returns-qcoreapplication-instance/td-p/9035295
+        import shiboken2
+        from PySide2 import QtCore
+        coreapp = QtCore.QCoreApplication.instance()
+        ptr = shiboken2.getCppPointer(coreapp)[0]
+        return shiboken2.wrapInstance(ptr, CutterApplication)
 except ImportError:
     pass
 
