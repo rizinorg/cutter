529c5a1c3088cbb97a1a81cf1dffc98ba615d66a
itayc0hen <itaycohen23@gmail.com>
2020-01-24 08:17:32 +0200

    remove method

--- src/widgets/FlagsWidget.h
+++ src/widgets/FlagsWidget.h
@@ -40,7 +40,6 @@ public:
 
     RVA address(const QModelIndex &index) const override;
     QString name(const QModelIndex &index) const override;
-    QString realname(const QModelIndex &index) const;
 
     const FlagDescription *description(QModelIndex index) const;
 };
