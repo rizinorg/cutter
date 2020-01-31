3451062cf3c1f775ac3d851b5d0fc423b2080a64
Florian Märkl <info@florianmaerkl.de>
2020-01-31 19:03:14 +0100

    Version 1.10.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The Cutter Developers'
 # The short X.Y version
 version = '1.10'
 # The full version, including alpha/beta/rc tags
-release = '1.10.0'
+release = '1.10.1'
 
 
 # -- General configuration ---------------------------------------------------
