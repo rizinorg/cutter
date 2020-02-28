c93681649b1a69a581efe3ce457dc3b4e295e41f
Itay Cohen <itaycohen23@gmail.com>
2020-02-28 10:37:07 +0200

    Change column oreder in xrefs (#2068)

--- src/dialogs/XrefsDialog.cpp
+++ src/dialogs/XrefsDialog.cpp
@@ -25,7 +25,7 @@ XrefsDialog::XrefsDialog(MainWindow *main, QWidget *parent) :
     ui->fromTreeWidget->setModel(&fromModel);
 
     // Modify the splitter's location to show more Disassembly instead of empty space. Not possible via Designer
-    ui->splitter->setSizes(QList<int>() << 100 << 200);
+    ui->splitter->setSizes(QList<int>() << 300 << 400);
 
     // Increase asm text edit margin
     QTextDocument *asm_docu = ui->previewTextEdit->document();
@@ -212,14 +212,14 @@ QVariant XrefModel::data(const QModelIndex &index, int role) const
         switch (index.column()) {
         case OFFSET:
             return to ? xref.from_str : xref.to_str;
+        case TYPE:
+            return xrefTypeString(xref.type);
         case CODE:
             if (to || xref.type != "DATA") {
                 return Core()->disassembleSingleInstruction(xref.from);
             } else {
                 return QString();
             }
-        case TYPE:
-            return xrefTypeString(xref.type);
         }
         return QVariant();
     case FlagDescriptionRole:
@@ -239,10 +239,10 @@ QVariant XrefModel::headerData(int section, Qt::Orientation orientation, int rol
         switch (section) {
         case OFFSET:
             return tr("Address");
-        case CODE:
-            return tr("Code");
         case TYPE:
             return tr("Type");
+        case CODE:
+            return tr("Code");
         default:
             return QVariant();
         }
