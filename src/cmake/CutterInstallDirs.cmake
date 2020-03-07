dbd3d928df44ab2f168f29335f677e68e8e77a01
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-03-07 19:29:48 +0200

    Fix filename on Linux.

--- src/cmake/CutterInstallDirs.cmake
+++ src/cmake/CutterInstallDirs.cmake
@@ -1,7 +1,7 @@
 if(WIN32)
     set(CMAKE_INSTALL_BINDIR "." CACHE PATH "Executable install directory")
 elseif(APPLE)
-    include(GnuInstallDirs) #TODO: use appropriate paths for macOS
+    include(GNUInstallDirs) #TODO: use appropriate paths for macOS
 else()
-    include(GnuInstallDirs)
+    include(GNUInstallDirs)
 endif()
