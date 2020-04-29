040b9e055d71975585880b209199ed00f9b47ffb
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-04-29 22:09:35 +0300

    Update version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The Cutter Developers'
 # The short X.Y version
 version = '1.10'
 # The full version, including alpha/beta/rc tags
-release = '1.10.3-pre'
+release = '1.10.3-pre2'
 
 
 # -- General configuration ---------------------------------------------------
