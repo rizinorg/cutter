a3374b20c718f4b256a0bf8935781503767f45ff
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-05-20 14:53:26 +0300

    Document the tree structures.

--- src/common/BinaryTrees.h
+++ src/common/BinaryTrees.h
@@ -1,58 +1,87 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+/** \file BinaryTrees.h
+ * \brief Utilities to simplify cration of specilized augmented binary trees.
+ */
+
 #include <vector>
 #include <cstdlib>
 #include <climits>
 #include <cstdint>
 #include <algorithm>
 
+
+/**
+ * Not really a segment tree for storing segments as referred in academic literature. Can be considered a
+ * full, almost perfect, augemented binary tree. In the context of competetive programming often called segment tree.
+ *
+ * Child classes are expected to implement updateFromChildren(NodeType&parent, NodeType& left, NodeType& right)
+ * method which calculates inner node values from children nodes.
+ *
+ * \tparam NodeTypeT type of each tree element
+ * \tparam FinalType final child class used for curiosly recurring template pattern
+ */
 template<class NodeTypeT, class FinalType>
 class SegmentTreeBase
 {
 public:
-    using NodeIndex = size_t;
+    using NodePosition = size_t;
     using NodeType = NodeTypeT;
 
+    /**
+     * @brief Create tree with \a size leaves.
+     * @param size numbe of leaves in the tree
+     */
     explicit SegmentTreeBase(size_t size)
         : size(size)
         , nodeCount(2 * size)
         , nodes(nodeCount)
     {}
 
+    /**
+     * @brief Create a tree with given size and initial value.
+     *
+     * Inner nodes are calculated from leaves.
+     * @param size number of leaves
+     * @param initialValue initial leave value
+     */
     SegmentTreeBase(size_t size, const NodeType &initialValue)
         : SegmentTreeBase(size)
     {
         init(initialValue);
     }
 protected:
-    // Curiously recurrent pattern
+    // Curiously recurring template pattern
     FinalType &This()
     {
         return static_cast<FinalType &>(*this);
     }
 
-    // Curiously recurrent pattern
+    // Curiously recurring template pattern
     const FinalType &This() const
     {
         return static_cast<const FinalType &>(*this);
     }
 
-    size_t leaveIndexToPosition(NodeIndex index) const
+    size_t leavePositionToIndex(NodePosition pos) const
     {
-        return index - size;
+        return pos - size;
     }
 
-    NodeIndex positionToLeaveIndex(size_t position) const
+    NodePosition leaveIndexToPosition(size_t index) const
     {
-        return position + size;
+        return index + size;
     }
 
-    bool isLeave(NodeIndex position) const
+    bool isLeave(NodePosition position) const
     {
         return position >= size;
     }
 
+    /**
+     * @brief Calculate inner node values from leaves.
+     */
     void buildInnerNodes()
     {
         for (size_t i = size - 1; i > 0; i--) {
@@ -60,6 +89,10 @@ protected:
         }
     }
 
+    /**
+     * @brief Initialize leaves with given value.
+     * @param value value that will be assigned to leaves
+     */
     void init(const NodeType &value)
     {
         std::fill_n(nodes.begin() + size, size, value);
@@ -71,6 +104,9 @@ protected:
     std::vector<NodeType> nodes;
 };
 
+/**
+ * \brief Tree for point modification and range queries.
+ */
 template<class NodeType, class FinalType>
 class PointSetSegmentTree : public SegmentTreeBase<NodeType, FinalType>
 {
@@ -78,9 +114,14 @@ class PointSetSegmentTree : public SegmentTreeBase<NodeType, FinalType>
 public:
     using BaseType::BaseType;
 
+    /**
+     * @brief Set leave \a index to \a value.
+     * @param index Leave index, should be in the range [0,size)
+     * @param value
+     */
     void set(size_t index, const NodeType &value)
     {
-        auto pos = this->positionToLeaveIndex(index);
+        auto pos = this->leaveIndexToPosition(index);
         this->nodes[pos] = value;
         while (pos > 1) {
             auto parrent = pos >> 1;
@@ -89,9 +130,9 @@ public:
         }
     }
 
-    const NodeType &valueAtPoint(size_t pos) const
+    const NodeType &valueAtPoint(size_t index) const
     {
-        return this->nodes[this->positionToLeaveIndex(pos)];
+        return this->nodes[this->leaveIndexToPosition(index)];
     }
 
     // Implement range query when necesarry
@@ -113,7 +154,7 @@ public:
     /**
      * @brief Find right most position with value than less than given in range [0; position].
      * @param position inclusive right side of query range
-     * @param value search for position with value less than this
+     * @param value search for position less than this
      * @return returns the position with searched property or -1 if there is no such position.
      */
     int rightMostLessThan(size_t position, int value)
@@ -123,7 +164,7 @@ public:
         };
         // right side exclusive range [l;r)
         size_t goodSubtree = 0;
-        for (size_t l = positionToLeaveIndex(0), r = positionToLeaveIndex(position + 1); l < r;
+        for (size_t l = leaveIndexToPosition(0), r = leaveIndexToPosition(position + 1); l < r;
                 l >>= 1, r >>= 1) {
             if (l & 1) {
                 if (isGood(l)) {
@@ -150,13 +191,13 @@ public:
                 goodSubtree ^= 1;
             }
         }
-        return leaveIndexToPosition(goodSubtree); // convert from node index to position in range (0;size]
+        return leavePositionToIndex(goodSubtree);
     }
 
     /**
-     * @brief Find left most position with value than less than given in range [position; size).
+     * @brief Find left most position with value less than \a value in range [position; size).
      * @param position inclusive left side of query range
-     * @param value search for position with value less than this
+     * @param value search for position less than this
      * @return returns the position with searched property or -1 if there is no such position.
      */
     int leftMostLessThan(size_t position, int value)
@@ -166,7 +207,7 @@ public:
         };
         // right side exclusive range [l;r)
         size_t goodSubtree = 0;
-        for (size_t l = positionToLeaveIndex(position), r = positionToLeaveIndex(size); l < r;
+        for (size_t l = leaveIndexToPosition(position), r = leaveIndexToPosition(size); l < r;
                 l >>= 1, r >>= 1) {
             if (l & 1) {
                 if (isGood(l)) {
@@ -193,15 +234,31 @@ public:
                 goodSubtree ^= 1;
             }
         }
-        return leaveIndexToPosition(goodSubtree); // convert from node index to position in range (0;size]
+        return leavePositionToIndex(goodSubtree);
     }
 };
 
+/**
+ * \brief Tree that supports lazily applying an operation to range.
+ *
+ * Each inner node has a promise value describing an operation that needs to be applied to corresponding subtree.
+ *
+ * Child classes are expected to implement to pushDown(size_t nodePosition) method. Which applies the applies the
+ * operation stored in \a promise for nodePosition to the direct children nodes.
+ *
+ * \tparam NodeType type of tree nodes
+ * \tparam PromiseType type describing operation that needs to be applied to subtree
+ * \tparam FinalType child class type for CRTP. See SegmentTreeBase
+ */
 template <class NodeType, class PromiseType, class FinalType>
 class LazySegmentTreeBase : public SegmentTreeBase<NodeType, FinalType>
 {
     using BaseType = SegmentTreeBase<NodeType, FinalType>;
 public:
+    /**
+     * @param size Number of tree leaves.
+     * @param neutralPromise Promise value that doesn't modify tree nodes.
+     */
     LazySegmentTreeBase(size_t size, const PromiseType &neutralPromise)
         : BaseType(size)
         , neutralPromiseElement(neutralPromise)
@@ -221,11 +278,18 @@ public:
         this->init(value);
     }
 
+    /**
+     * @brief Calculate the tree operation over the range [\a l, \a r)
+     * @param l inclusive range left side
+     * @param r exclusive range right side
+     * @param initialValue Initial value for aggregate operation.
+     * @return Tree operation calulated over the range.
+     */
     NodeType rangeOperation(size_t l, size_t r, NodeType initialValue)
     {
         NodeType result = initialValue;
-        l = this->positionToLeaveIndex(l);
-        r = this->positionToLeaveIndex(r);
+        l = this->leaveIndexToPosition(l);
+        r = this->leaveIndexToPosition(r);
         pushDownFromRoot(l);
         pushDownFromRoot(r - 1);
         for (; l < r; l >>= 1, r >>= 1) {
@@ -240,14 +304,22 @@ public:
     }
 
 protected:
-    void pushDownFromRoot(size_t p)
+    /**
+     * @brief Ensure that all the parents of node \a p have the operation applied.
+     * @param p Node position
+     */
+    void pushDownFromRoot(typename BaseType::NodePosition p)
     {
         for (size_t i = h; i > 0; i--) {
             This().pushDown(p >> i);
         }
     }
 
-    void updateUntilRoot(size_t p)
+    /**
+     * @brief Update all the inner nodes in path from \a p to root.
+     * @param p node position
+     */
+    void updateUntilRoot(typename BaseType::NodePosition p)
     {
         while (p > 1) {
             auto parent = p >> 1;
@@ -260,11 +332,15 @@ protected:
 
     using BaseType::This;
 
-    int h;
+    int h; //< Tree height
     const PromiseType neutralPromiseElement;
     std::vector<PromiseType> promise;
 };
 
+
+/**
+ * @brief Structure supporting range assignment and range maximum operations.
+ */
 class RangeAssignMaxTree : public LazySegmentTreeBase<int, uint8_t, RangeAssignMaxTree>
 {
     using BaseType = LazySegmentTreeBase<int, uint8_t, RangeAssignMaxTree>;
@@ -296,10 +372,16 @@ public:
         }
     }
 
+    /**
+     * @brief Change all the elements in range [\a left, \a right) to \a value.
+     * @param left incluseive range leftside
+     * @param right exclusive right side of range
+     * @param value value to be assigned
+     */
     void setRange(size_t left, size_t right, NodeType value)
     {
-        left = positionToLeaveIndex(left);
-        right = positionToLeaveIndex(right);
+        left = leaveIndexToPosition(left);
+        right = leaveIndexToPosition(right);
         pushDownFromRoot(left);
         pushDownFromRoot(right - 1);
         for (size_t l = left, r = right; l < r; l >>= 1, r >>= 1) {
@@ -322,6 +404,12 @@ public:
         updateUntilRoot(right - 1);
     }
 
+    /**
+     * @brief Calculate biggest value in the range [l, r)
+     * @param l inclusive left side of range
+     * @param r exclusive right side of range
+     * @return biggest value in given range
+     */
     int rangeMaximum(size_t l, size_t r)
     {
         return rangeOperation(l, r, std::numeric_limits<ValueType>::min());
