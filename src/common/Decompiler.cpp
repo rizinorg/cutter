ed3a33d2ef99c4418dfdb0071929187a429b933b
NIRMAL MANOJ C <nimmumanoj@gmail.com>
2020-06-01 02:10:59 +0530

    changed the name of a new variable in decompileAt()

--- src/common/Decompiler.cpp
+++ src/common/Decompiler.cpp
@@ -66,7 +66,7 @@ void R2DecDecompiler::decompileAt(RVA addr)
     }
     task = new R2Task("pddj @ " + QString::number(addr));
     connect(task, &R2Task::finished, this, [this]() {
-        RAnnotatedCode *codi = r_annotated_code_new (nullptr);
+        RAnnotatedCode *code = r_annotated_code_new (nullptr);
         QString codeString = "";
         QJsonObject json = task->getResultJson().object();
         delete task;
@@ -74,8 +74,8 @@ void R2DecDecompiler::decompileAt(RVA addr)
         if (json.isEmpty()) {
             codeString = tr("Failed to parse JSON from r2dec");
             QByteArray ba = codeString.toUtf8();
-            codi->code = ba.data();
-            emit finished(codi);
+            code->code = ba.data();
+            emit finished(code);
             return;
         }
 
@@ -99,7 +99,7 @@ void R2DecDecompiler::decompileAt(RVA addr)
             bool ok;
             annotationi->type = R_CODE_ANNOTATION_TYPE_OFFSET;
             annotationi->offset.offset = lineObject["offset"].toVariant().toULongLong(&ok);
-            r_annotated_code_add_annotation (codi, annotationi);
+            r_annotated_code_add_annotation (code, annotationi);
         }
 
         for (const auto &line : json["errors"].toArray()) {
@@ -109,8 +109,8 @@ void R2DecDecompiler::decompileAt(RVA addr)
             codeString.append(line.toString() + "\n");
         }
         QByteArray ba = codeString.toUtf8();
-        codi->code = ba.data();
-        emit finished(codi);
+        code->code = ba.data();
+        emit finished(code);
     });
     task->startTask();
 }
