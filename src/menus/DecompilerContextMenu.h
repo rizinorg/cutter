a2d3917f01eef88425d6d09e2f1d4e29df1bbd4d
NIRMAL MANOJ C <nimmumanoj@gmail.com>
2020-06-30 00:38:02 +0530

    Breakpoint Menu and Debug Menu in the Decompiler Context Menu (#2260)

--- src/menus/DecompilerContextMenu.h
+++ src/menus/DecompilerContextMenu.h
@@ -13,33 +13,71 @@ public:
     DecompilerContextMenu(QWidget *parent, MainWindow *mainWindow);
     ~DecompilerContextMenu();
 
+    bool getIsTogglingBreakpoints();
+
 signals:
     void copy();
 
 public slots:
     void setOffset(RVA offset);
     void setCanCopy(bool enabled);
+    void setFirstOffsetInLine(RVA firstOffset);
+    void setAvailableBreakpoints(QVector<RVA> offsetList);
+
 
 private slots:
     void aboutToShowSlot();
 
     void actionCopyTriggered();
 
-private:
-    QKeySequence getCopySequence() const;
+    void actionToggleBreakpointTriggered();
+    void actionAdvancedBreakpointTriggered();
 
+    void actionContinueUntilTriggered();
+    void actionSetPCTriggered();
+
+private:
+    // Private variables
     RVA offset;
+    RVA firstOffsetInLine;
+    bool isTogglingBreakpoints;
+    QVector<RVA> availableBreakpoints;
     MainWindow *mainWindow;
 
     QAction actionCopy;
     QAction *copySeparator;
-    
+
+    QMenu *breakpointMenu;
+    QAction actionToggleBreakpoint;
+    QAction actionAdvancedBreakpoint;
+
+    QMenu *breakpointsInLineMenu;
+
+    QMenu *debugMenu;
+    QAction actionContinueUntil;
+    QAction actionSetPC;
+
+    // Private Functions
+    void setShortcutContextInActions(QMenu *menu);
+    void setupBreakpointsInLineMenu();
+    void setIsTogglingBreakpoints(bool isToggling);
+
+    // Set actions
     void setActionCopy();
-    
+
+    void setActionToggleBreakpoint();
+    void setActionAdvancedBreakpoint();
+
+    void setActionContinueUntil();
+    void setActionSetPC();
+
+    // Add Menus
+    void addBreakpointMenu();
+    void addDebugMenu();
     // I left out the following part from RAnnotatedCode. Probably, we will be returning/passing annotations
     // from/to the function getThingUsedHere() and updateTargetMenuActions(). This block of comment will get removed in
     // future PRs.
-    // 
+    //
     // struct ThingUsedHere {
     //     QString name;
     //     RVA offset;
