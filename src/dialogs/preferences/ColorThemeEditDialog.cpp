86d67b256564618e018b59f3f7d7dbc927bf9d29
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-08-02 13:46:32 +0300

    Prevent potential crash when opening theme editor dialog.

--- src/dialogs/preferences/ColorThemeEditDialog.cpp
+++ src/dialogs/preferences/ColorThemeEditDialog.cpp
@@ -31,7 +31,9 @@ ColorThemeEditDialog::ColorThemeEditDialog(QWidget *parent) :
     previewDisasmWidget->setPreviewMode(true);
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 12, 0))
     // default size limit is acceptable
-    previewDisasmWidget->setMinimumSize(qApp->screenAt(previewDisasmWidget->pos())->size() * 0.5);
+    if (auto screen = qApp->screenAt(previewDisasmWidget->pos())) {
+        previewDisasmWidget->setMinimumSize(screen->size() * 0.5);
+    }
 #endif
     previewDisasmWidget->setWindowTitle(tr("Disassembly Preview"));
     previewDisasmWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
