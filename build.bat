724d3f26b4020d90dc9103e7f6ee19f74b3a1577
karliss <karlis3p70l1ij@gmail.com>
2020-08-08 18:43:43 +0300

    Adjust r2 paths (#2338)

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ IF !ERRORLEVEL! NEQ 0 (
     EXIT /B 1
 )
 
-SET "R2DIST=r2_dist_%PLATFORM%"
+SET "R2DIST=r2_dist"
 SET "BUILDDIR=build_%PLATFORM%"
 SET "BREAKPAD_SOURCE_DIR=%CD%\src\breakpad\src\src"
 
@@ -36,8 +36,9 @@ IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 ECHO Deploying cutter
 MKDIR cutter
 COPY release\cutter.exe cutter\cutter.exe
-XCOPY /S /I ..\%R2DIST%\radare2 cutter\radare2
-DEL cutter\radare2\lib\*.lib
+XCOPY /S /I ..\%R2DIST%\share cutter\share
+XCOPY /S /I ..\%R2DIST%\lib cutter\lib
+DEL cutter\lib\*.lib
 COPY ..\%R2DIST%\bin\*.dll cutter\
 windeployqt cutter\cutter.exe
 FOR %%i in (..\src\translations\*.qm) DO MOVE "%%~fi" cutter\translations
