6f137e562c89a4fd690bb72cb36e72e64d5e6509
karliss <karlis3p70l1ij@gmail.com>
2020-08-11 16:19:37 +0300

    Lock the decompiler versions used. (#2382)

--- scripts/bundle_r2dec.ps1
+++ scripts/bundle_r2dec.ps1
@@ -1,7 +1,7 @@
 $dist = $args[0]
 $python = Split-Path((Get-Command python.exe).Path)
 
-git clone https://github.com/wargio/r2dec-js.git
+git clone --depth 1 https://github.com/radareorg/r2dec-js.git --branch 4.5.0
 cd r2dec-js
 & meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
 ninja -C build install
