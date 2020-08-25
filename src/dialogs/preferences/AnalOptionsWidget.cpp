8f744283b11a8635f52068bf83606c9a8aa68bfb
ocastejon <oriol.castejon@gmail.com>
2020-08-25 20:45:18 +0200

    Add more options to the Analysis widget

--- src/dialogs/preferences/AnalOptionsWidget.cpp
+++ src/dialogs/preferences/AnalOptionsWidget.cpp
@@ -8,6 +8,18 @@
 
 #include "core/MainWindow.h"
 
+#include <QComboBox>
+
+static const QHash<QString, QString> analBoundaries {
+    {"io.maps.x", "All executable maps"},
+    {"io.maps", "All maps"},
+    {"io.map", "Current map"},
+    {"raw", "Raw"},
+    {"block", "Current block"},
+    {"bin.section", "Current mapped section"},
+    {"bin.sections", "All mapped sections"},
+};
+
 AnalOptionsWidget::AnalOptionsWidget(PreferencesDialog *dialog)
     : QDialog(dialog),
       ui(new Ui::AnalOptionsWidget)
@@ -24,6 +36,9 @@ AnalOptionsWidget::AnalOptionsWidget(PreferencesDialog *dialog)
         { ui->verboseCheckBox,      "anal.verbose" }
     };
 
+    // Create list of options for the anal.in selector
+    createAnalInOptionsList();
+
     // Connect each checkbox from "checkboxes" to the generic signal "checkboxEnabler"
     for (ConfigCheckbox &confCheckbox : checkboxes) {
         QString val = confCheckbox.config;
@@ -33,7 +48,8 @@ AnalOptionsWidget::AnalOptionsWidget(PreferencesDialog *dialog)
 
     ui->analyzePushButton->setToolTip("Analyze the program using radare2's \"aaa\" command");
     auto *mainWindow = new MainWindow(this);
-    connect(ui->analyzePushButton, &QPushButton::clicked, mainWindow, &MainWindow::on_actionAnalyze_triggered);
+    connect(ui->analyzePushButton, &QPushButton::clicked, mainWindow,
+            &MainWindow::on_actionAnalyze_triggered);
     updateAnalOptionsFromVars();
 }
 
@@ -49,4 +65,37 @@ void AnalOptionsWidget::updateAnalOptionsFromVars()
     for (ConfigCheckbox &confCheckbox : checkboxes) {
         qhelpers::setCheckedWithoutSignals(confCheckbox.checkBox, Core()->getConfigb(confCheckbox.config));
     }
+
+    // Update the rest of analysis options that are not checkboxes
+    ui->analInComboBox->setCurrentIndex(ui->analInComboBox->findData(Core()->getConfig("anal.in")));
+    ui->ptrdepthSpinBox->setValue(Core()->getConfigi("anal.ptrdepth"));
+    ui->preludeLineEdit->setText(Core()->getConfig("anal.prelude"));
+}
+
+void AnalOptionsWidget::on_analInComboBox_currentIndexChanged(int index)
+{
+    Config()->setConfig("anal.in", ui->analInComboBox->itemData(index).toString());
+}
+
+void AnalOptionsWidget::on_ptrdepthSpinBox_valueChanged(int value)
+{
+    Config()->setConfig("anal.ptrdepth", value);
+}
+
+void AnalOptionsWidget::on_preludeLineEdit_textChanged(const QString &prelude)
+{
+    Config()->setConfig("anal.prelude", prelude);
+}
+
+void AnalOptionsWidget::createAnalInOptionsList()
+{
+    QHash<QString, QString>::const_iterator mapIter;
+
+    mapIter = analBoundaries.cbegin();
+    ui->analInComboBox->blockSignals(true);
+    ui->analInComboBox->clear();
+    for (; mapIter != analBoundaries.cend(); ++mapIter) {
+        ui->analInComboBox->addItem(mapIter.value(), mapIter.key());
+    }
+    ui->analInComboBox->blockSignals(false);
 }
