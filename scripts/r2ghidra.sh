be12536b62ceea11024b47d3f57e311b72608f8b
karliss <karlis3p70l1ij@gmail.com>
2020-09-05 12:56:45 +0300

    Fix r2ghidra repository name.

--- scripts/r2ghidra.sh
+++ scripts/r2ghidra.sh
@@ -6,13 +6,13 @@ SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 cd "$SCRIPTPATH/.."
 
-if [[ ! -d r2ghidra-dec ]]; then
-    git clone --recurse-submodules https://github.com/karliss/r2ghidra-dec.git || exit 1
-    pushd r2ghidra-dec
+if [[ ! -d r2ghidra ]]; then
+    git clone --recurse-submodules https://github.com/karliss/r2ghidra.git || exit 1
+    pushd r2ghidra
     git checkout  --recurse-submodules 5e845f4b50e8559bd51af03b22b6586e8cc5c35c
     popd
 fi
-cd r2ghidra-dec || exit 1
+cd r2ghidra || exit 1
 
 mkdir build && cd build || exit 1
 cmake -G Ninja "$@" .. || exit 1
