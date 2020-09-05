1966c76ba7a9146761a6e2ef5c54574ad962a08c
karliss <karlis3p70l1ij@gmail.com>
2020-09-05 11:25:34 +0300

    Update windows CMake breakpad code. (#2421)

--- src/cmake/FindBreakpad.cmake
+++ src/cmake/FindBreakpad.cmake
@@ -1,46 +1,60 @@
 # - Find Breakpad
 #
-#  BREAKPAD_FOUND          - True if Breakpad has been found.
-#  BREAKPAD_INCLUDE_DIRS   - Breakpad include directory
-#  BREAKPAD_LIBRARIES      - List of libraries when using Breakpad.
-#  BREAKPAD_LIBRARY_DIRS   - Breakpad library directories
+#  Breakpad_FOUND          - True if Breakpad has been found.
+#  Breakpad_INCLUDE_DIRS   - Breakpad include directory
+#  Breakpad_LIBRARIES      - List of libraries when using Breakpad.
 
+set(Breakpad_LIBRARIES_VARS "")
 if(WIN32)
-	find_path(BREAKPAD_INCLUDE_DIRS
+	find_path(Breakpad_INCLUDE_DIRS
+			client/windows/handler/exception_handler.h
 			HINTS
-				"${CMAKE_CURRENT_SOURCE_DIR}/breakpad/prefix/include/breakpad")
+				"${CMAKE_CURRENT_SOURCE_DIR}/Breakpad/src/src")
 
-	set(BREAKPAD_LIBRARY_NAMES
-			BREAKPAD_CLIENT
-			BREAKPAD)
+	set(Breakpad_LIBRARY_NAMES
+			exception_handler
+			crash_generation_client
+			common
+			)
 
-	set(BREAKPAD_LIBRARIES "")
-	set(BREAKPAD_LIBRARIES_VARS "")
-	foreach(libname ${BREAKPAD_LIBRARY_NAMES})
-		find_library(BREAKPAD_LIBRARY_${libname}
+	set(Breakpad_LIBRARIES "")
+	
+	foreach(libname ${Breakpad_LIBRARY_NAMES})
+		find_library(Breakpad_LIBRARY_${libname}
 				${libname}
 				HINTS
-					"${CMAKE_CURRENT_SOURCE_DIR}/breakpad/prefix/lib")
+					"${CMAKE_CURRENT_SOURCE_DIR}/Breakpad/src/src/client/windows/Release/lib"
+				REQUIRED)
 
-		list(APPEND BREAKPAD_LIBRARIES ${BREAKPAD_LIBRARY_${libname}})
-		list(APPEND BREAKPAD_LIBRARIES_VARS "BREAKPAD_LIBRARY_${libname}")
+		list(APPEND Breakpad_LIBRARIES ${Breakpad_LIBRARY_${libname}})
+		list(APPEND Breakpad_LIBRARIES_VARS "Breakpad_LIBRARY_${libname}")
 	endforeach()
+	
+	set (Breakpad_LINK_LIBRARIES ${Breakpad_LIBRARIES})
 
-	set(BREAKPAD_LIBRARY_DIRS "")
+	set(Breakpad_LIBRARY_DIRS "")
 else()
-	set(BREAKPAD_CMAKE_PREFIX_PATH_TEMP ${CMAKE_PREFIX_PATH})
-	list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/breakpad/prefix")
+	set(Breakpad_CMAKE_PREFIX_PATH_TEMP ${CMAKE_PREFIX_PATH})
+	list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Breakpad/prefix")
 
 	find_package(PkgConfig REQUIRED)
-	pkg_search_module(BREAKPAD REQUIRED breakpad-client)
+	pkg_search_module(Breakpad REQUIRED breakpad-client)
 
 	# reset CMAKE_PREFIX_PATH
-	set(CMAKE_PREFIX_PATH ${BREAKPAD_CMAKE_PREFIX_PATH_TEMP})
-	mark_as_advanced(BREAKPAD_CMAKE_PREFIX_PATH_TEMP)
+	set(CMAKE_PREFIX_PATH ${Breakpad_CMAKE_PREFIX_PATH_TEMP})
+	mark_as_advanced(Breakpad_CMAKE_PREFIX_PATH_TEMP)
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BREAKPAD REQUIRED_VARS BREAKPAD_LIBRARIES BREAKPAD_INCLUDE_DIRS)
+# could be simplified in > cmake 3.11 using pkg_search_module IMPORTED_TARGET [GLOBAL] but this would still be required for windows
+add_library(Breakpad::client INTERFACE IMPORTED)
+set_target_properties(Breakpad::client PROPERTIES
+	INTERFACE_INCLUDE_DIRECTORIES "${Breakpad_INCLUDE_DIRS}")
+set_target_properties(Breakpad::client PROPERTIES
+	INTERFACE_LINK_LIBRARIES "${Breakpad_LINK_LIBRARIES}")
 
-mark_as_advanced(BREAKPAD_LIBRARIES_VARS)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Breakpad REQUIRED_VARS Breakpad_LIBRARIES Breakpad_INCLUDE_DIRS ${Breakpad_LIBRARIES_VARS})
+
+mark_as_advanced(Breakpad_LIBRARIES_VARS)
  
