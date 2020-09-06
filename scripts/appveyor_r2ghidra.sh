ad66718f375b013a3021e531891c71082ee48867
karliss <karlis3p70l1ij@gmail.com>
2020-09-06 11:52:13 +0300

    Add CMake code for packaging r2ghidra on windows. (#2422)

--- scripts/appveyor_r2ghidra.sh
+++ scripts/appveyor_r2ghidra.sh
@@ -2,14 +2,10 @@
 
 set -eu
 
-git clone --recurse-submodules https://github.com/radareorg/r2ghidra-dec.git $APPVEYOR_BUILD_FOLDER/r2ghidra-dec
-pushd $APPVEYOR_BUILD_FOLDER/r2ghidra-dec
-git checkout --recurse-submodules 8e576eeadc211de4ac8d8c759cc368fa48cdfa99
-popd
-
 scripts/r2ghidra.sh \
 	-DCMAKE_C_COMPILER=cl \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_PREFIX_PATH="$APPVEYOR_BUILD_FOLDER/r2_dist;$APPVEYOR_BUILD_FOLDER/r2_dist/include/libr;$APPVEYOR_BUILD_FOLDER/r2_dist/include/libr/sdb" \
 	-DCMAKE_INSTALL_PREFIX="$APPVEYOR_BUILD_FOLDER/r2_dist" \
-	-DRADARE2_INSTALL_PLUGDIR="$APPVEYOR_BUILD_FOLDER/r2_dist/lib/plugins"
+	-DRADARE2_INSTALL_PLUGDIR="$APPVEYOR_BUILD_FOLDER/r2_dist/lib/plugins" \
+	-DBUILD_SLEIGH_PLUGIN=OFF
--- scripts/r2ghidra.sh
+++ scripts/r2ghidra.sh
@@ -6,13 +6,14 @@ SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 cd "$SCRIPTPATH/.."
 
-if [[ ! -d r2ghidra-dec ]]; then
-    git clone --recurse-submodules https://github.com/radareorg/r2ghidra-dec.git || exit 1
-    pushd r2ghidra-dec
-    git checkout  --recurse-submodules 8e576eeadc211de4ac8d8c759cc368fa48cdfa99
-    popd
+if [[ ! -d r2ghidra ]]; then
+    # remove depth, branch and uncomment lines bellow to use specifc commit
+    git clone --recurse-submodules https://github.com/radareorg/r2ghidra.git  --depth 1  --branch master || exit 1
+    #pushd r2ghidra
+    #git checkout  --recurse-submodules 5e845f4b50e8559bd51af03b22b6586e8cc5c35c
+    #popd
 fi
-cd r2ghidra-dec || exit 1
+cd r2ghidra || exit 1
 
 mkdir build && cd build || exit 1
 cmake -G Ninja "$@" .. || exit 1
