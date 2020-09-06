4dd68dae77382dd075d4442db6c8411e3d9d5811
karliss <karlis3p70l1ij@gmail.com>
2020-09-06 21:10:41 +0300

    Add cmake script for packaging r2dec on windows.

--- scripts/bundle_r2dec.ps1
+++ scripts/bundle_r2dec.ps1
@@ -1,9 +1,11 @@
 $dist = $args[0]
 $python = Split-Path((Get-Command python.exe).Path)
 
-git clone https://github.com/radareorg/r2dec-js.git
+if (-not (Test-Path -Path 'r2dec-js' -PathType Container)) {
+    git clone https://github.com/radareorg/r2dec-js.git --branch master --depth 1
+}
 cd r2dec-js
-git checkout b5a0d15c7bcc488f268ffb0931b7ced2919f6c9d
+#git checkout b5a0d15c7bcc488f268ffb0931b7ced2919f6c9d
 & meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
 ninja -C build install
 Remove-Item -Recurse -Force $dist\lib\plugins\core_pdd.lib
--- src/cmake/Packaging.cmake
+++ src/cmake/Packaging.cmake
@@ -12,47 +12,58 @@ if(WIN32)
             install(FILES ${CUTTER_DEPS}/pyside/bin/shiboken2.abi3.dll ${CUTTER_DEPS}/pyside/bin/pyside2.abi3.dll DESTINATION .)
         endif()
         install(SCRIPT cmake/WindowsBundleQt.cmake)
-
-        if (CUTTER_PACKAGE_R2GHIDRA)
-            if (CUTTER_ENABLE_DEPENDENCY_DOWNLOADS)
-                # Currently using external project only for downloading source
-                # It neeeds to link against compiled cutter so for now build it using custom install script.
-                # That way r2ghidra build process is the same as with any other external plugin built against
-                # installed Cutter.
-                ExternalProject_Add(R2Ghidra
-                    GIT_REPOSITORY https://github.com/radareorg/r2ghidra.git
-                    GIT_TAG master
-                    CONFIGURE_COMMAND ""
-                    BUILD_COMMAND ""
-                    INSTALL_COMMAND ""
-                )
-            endif()
-            install(CODE "
-                execute_process(
-                    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/R2Ghidra-prefix/src/R2Ghidra-build
-                    RESULT_VARIABLE SCRIPT_RESULT
-                    COMMAND \${CMAKE_COMMAND} 
-                                    ../R2Ghidra
-                                    -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=\"\${CMAKE_INSTALL_PREFIX}\;\${CMAKE_INSTALL_PREFIX}/include/libr\;\${CMAKE_INSTALL_PREFIX}/include/libr/sdb\"
-                                    -DCMAKE_INSTALL_PREFIX=\${CMAKE_INSTALL_PREFIX}
-                                    -DRADARE2_INSTALL_PLUGDIR=\${CMAKE_INSTALL_PREFIX}/lib/plugins
-                                    -DCUTTER_INSTALL_PLUGDIR=plugins/native
-                                    -DBUILD_CUTTER_PLUGIN=ON
-                                    -DBUILD_SLEIGH_PLUGIN=OFF
-                                    -G Ninja
-                    )
-                if (SCRIPT_RESULT)
-                    message(FATAL_ERROR \"Failed to configure r2ghidra\")
-                endif()
-                execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/R2Ghidra-prefix/src/R2Ghidra-build
-                    RESULT_VARIABLE SCRIPT_RESULT
-                    COMMAND \${CMAKE_COMMAND} --build . --target install
-                    )
-                if (SCRIPT_RESULT)
-                    message(FATAL_ERROR \"Failed to install r2ghidra plugin\")
-                endif()
-            ")
+     endif()
+    if (CUTTER_PACKAGE_R2GHIDRA)
+        if (CUTTER_ENABLE_DEPENDENCY_DOWNLOADS)
+            # Currently using external project only for downloading source
+            # It neeeds to link against compiled cutter so for now build it using custom install script.
+            # That way r2ghidra build process is the same as with any other external plugin built against
+            # installed Cutter.
+            ExternalProject_Add(R2Ghidra
+                GIT_REPOSITORY https://github.com/radareorg/r2ghidra.git
+                GIT_TAG master
+                CONFIGURE_COMMAND ""
+                BUILD_COMMAND ""
+                INSTALL_COMMAND ""
+            )
         endif()
+        install(CODE "
+            execute_process(
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/R2Ghidra-prefix/src/R2Ghidra-build
+                RESULT_VARIABLE SCRIPT_RESULT
+                COMMAND \${CMAKE_COMMAND} 
+                                ../R2Ghidra
+                                -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=\"\${CMAKE_INSTALL_PREFIX}\;\${CMAKE_INSTALL_PREFIX}/include/libr\;\${CMAKE_INSTALL_PREFIX}/include/libr/sdb\"
+                                -DCMAKE_INSTALL_PREFIX=\${CMAKE_INSTALL_PREFIX}
+                                -DRADARE2_INSTALL_PLUGDIR=\${CMAKE_INSTALL_PREFIX}/lib/plugins
+                                -DCUTTER_INSTALL_PLUGDIR=plugins/native
+                                -DBUILD_CUTTER_PLUGIN=ON
+                                -DBUILD_SLEIGH_PLUGIN=OFF
+                                -G Ninja
+                )
+            if (SCRIPT_RESULT)
+                message(FATAL_ERROR \"Failed to configure r2ghidra\")
+            endif()
+            execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/R2Ghidra-prefix/src/R2Ghidra-build
+                RESULT_VARIABLE SCRIPT_RESULT
+                COMMAND \${CMAKE_COMMAND} --build . --target install
+                )
+            if (SCRIPT_RESULT)
+                message(FATAL_ERROR \"Failed to install r2ghidra plugin\")
+            endif()
+        ")
+    endif()
+    if (CUTTER_PACKAGE_R2DEC AND CUTTER_ENABLE_DEPENDENCY_DOWNLOADS)
+        install(CODE "
+            set(ENV{R_ALT_SRC_DIR} 1)
+            set(ENV{PATH} \"\${CMAKE_INSTALL_PREFIX};\$ENV{PATH}\")
+            execute_process(COMMAND powershell ../scripts/bundle_r2dec.ps1 \"\${CMAKE_INSTALL_PREFIX}\"
+                            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                            RESULT_VARIABLE SCRIPT_RESULT)
+            if (SCRIPT_RESULT)
+                message(FATAL_ERROR \"Failed to package r2dec\")
+            endif()
+        ")
     endif()
 endif()
 include(CPack)
