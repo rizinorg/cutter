c49cbc9f01f10f132756a3a838d73c864cda1000
karliss <karlis3p70l1ij@gmail.com>
2020-10-03 13:50:57 +0300

    Add cmake script for packaging r2dec on windows. (#2425)

--- scripts/r2dec.sh
+++ scripts/r2dec.sh
@@ -7,10 +7,10 @@ SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 cd "$SCRIPTPATH/.."
 
 if [[ ! -d r2dec-js ]]; then
-	git clone https://github.com/wargio/r2dec-js.git
-	cd r2dec-js
-	git checkout b5a0d15c7bcc488f268ffb0931b7ced2919f6c9d
-	cd ..
+	git clone https://github.com/radareorg/r2dec-js.git --depth 1 --branch master
+	#cd r2dec-js
+	#git checkout b5a0d15c7bcc488f268ffb0931b7ced2919f6c9d
+	#cd ..
 fi
 
 cd r2dec-js
