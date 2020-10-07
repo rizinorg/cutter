a89c5a744e3533f7f3983ca3813188277bf0cd87
xarkes <antide.petit@gmail.com>
2020-10-07 20:57:54 +0200

    Updated Cutter.pro to refer to proper translation

--- scripts/deploy_translations.sh
+++ scripts/deploy_translations.sh
@@ -11,11 +11,11 @@ git pull origin master
 
 # Generate Crowdin single translation file from cutter_fr.ts
 lupdate ../Cutter.pro
-mv cutter_fr.ts Translations.ts
+cp ./fr/cutter_fr_FR.ts ./Translations.ts
 
 # Push it so Crowdin can find new strings, and later push updated translations
 git add Translations.ts
 git commit -m "Updated translations"
 export GIT_SSH_COMMAND="/usr/bin/ssh -i $TRAVIS_BUILD_DIR/scripts/deploy_translations_rsa"
 echo "Pushing new translation strings..."
-git push -f "git@github.com:radareorg/cutter-translations.git" HEAD:refs/heads/master
+git push "git@github.com:radareorg/cutter-translations.git" HEAD:refs/heads/master
