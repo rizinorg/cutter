e282751b17a4c9607dc0cfab2463a350af12983e
yossizap <yossizap@gmail.com>
2020-10-31 21:32:09 +0000

    Stop existing trace sessions in stopDebug

--- src/core/Cutter.cpp
+++ src/core/Cutter.cpp
@@ -1868,7 +1868,7 @@ void CutterCore::stopDebug()
     emit debugTaskStateChanged();
 
     if (currentlyEmulating) {
-        cmdEsil("aeim-; aei-; wcr; .ar-");
+        cmdEsil("aeim-; aei-; wcr; .ar-; aets-");
         currentlyEmulating = false;
     } else if (currentlyAttachedToPID != -1) {
         // Use cmd because cmdRaw would not work with command concatenation
