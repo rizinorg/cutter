5abc3427ee36c62df5fbabe0b9fa1874d8ebe819
xarkes <antide.petit@gmail.com>
2020-11-11 14:05:21 +0100

    Make deploy_translations script more verbose (#2471)

--- scripts/deploy_translations.sh
+++ scripts/deploy_translations.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
+#########
+#### Push new translation files to cutter-translations
+#### so Crowdin can fetch them
 
-#######################
-#### Push new translation files
+log() {
+    echo "[TRANSLATIONS] $1"
+}
+
+log "Script started"
 
 # Update submodule
+log "Updating translations..."
 cd ${TRAVIS_BUILD_DIR}/src
 git submodule update translations
 cd translations
 git pull origin master
 
 # Generate Crowdin single translation file from cutter_fr.ts
+log "Generating single translation file"
 lupdate ../Cutter.pro
 cp ./fr/cutter_fr_FR.ts ./Translations.ts
 
 # Push it so Crowdin can find new strings, and later push updated translations
+log "Committing..."
 git add Translations.ts
 git commit -m "Updated translations"
+log "Pushing..."
 export GIT_SSH_COMMAND="/usr/bin/ssh -i $TRAVIS_BUILD_DIR/scripts/deploy_translations_rsa"
-echo "Pushing new translation strings..."
 git push "git@github.com:radareorg/cutter-translations.git" HEAD:refs/heads/master
+
+log "Script done!"
