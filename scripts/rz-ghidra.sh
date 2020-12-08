822e377383f1016a30d550d4a20f4bc0af92099c
Florian Märkl <info@florianmaerkl.de>
2020-12-08 17:27:54 +0100

    Revert Temporary Changes (#2496)

--- scripts/rz-ghidra.sh
+++ scripts/rz-ghidra.sh
@@ -7,10 +7,9 @@ SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 cd "$SCRIPTPATH/.."
 
 if [[ ! -d rz-ghidra ]]; then
-    # remove depth, branch and uncomment lines bellow to use specifc commit
+    # remove depth, branch and uncomment lines below to use specifc commit
 
-    git clone --recurse-submodules git@github.com:rizinorg/rz-ghidra.git  --depth 1  --branch master || exit 1
-    # git clone --recurse-submodules https://github.com/rizinorg/rz-ghidra.git  --depth 1  --branch master || exit 1
+    git clone --recurse-submodules https://github.com/rizinorg/rz-ghidra.git --depth 1 --branch master || exit 1
 
     #pushd rz-ghidra
     #git checkout  --recurse-submodules 5e845f4b50e8559bd51af03b22b6586e8cc5c35c
