e59a1d3962a72f66481cf64c867f1985773510ec
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-12-21 23:59:46 +0200

    Shell scripting fixes and error handling improvements.

--- scripts/prepare_breakpad_macos.sh
+++ scripts/prepare_breakpad_macos.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
+set -euo pipefail
+
 SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 DIR="$SCRIPTPATH/.."
-cd "$DIR" || exit 1
+cd "$DIR"
 BREAKPAD_FRAMEWORK_DIR="$DIR/breakpad/framework"
 BREAKPAD_DUMP_SYMS_DIR="$DIR/breakpad/bin"
-git clone https://github.com/google/breakpad.git || exit 1
+git clone https://github.com/google/breakpad.git
 mkdir $BREAKPAD_FRAMEWORK_DIR
 mkdir $BREAKPAD_DUMP_SYMS_DIR
-cd breakpad || exit 1
-git checkout 4d550cceca107f36c4bc1ea1126b7d32cc50f424 || exit 1
-git apply "$SCRIPTPATH/breakpad_macos.patch" || exit 1
-cd src/client/mac/ && xcodebuild -sdk macosx || exit 1
-cp -R build/Release/Breakpad.framework "$BREAKPAD_FRAMEWORK_DIR" || exit 1
+cd breakpad
+git checkout 4d550cceca107f36c4bc1ea1126b7d32cc50f424
+git apply "$SCRIPTPATH/breakpad_macos.patch"
+cd src/client/mac/ && xcodebuild -sdk macosx
+cp -R build/Release/Breakpad.framework "$BREAKPAD_FRAMEWORK_DIR"
 
-cd $DIR/breakpad || exit 1
-cp -R src/. framework/Breakpad.framework/Headers || exit 1
+cd $DIR/breakpad
+cp -R src/. framework/Breakpad.framework/Headers
 
 export BREAKPAD_FRAMEWORK_DIR=$BREAKPAD_FRAMEWORK_DIR
 cd $DIR
