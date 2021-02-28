6cbf0c0cbf1853dab88fe0d11a0db931826551d4
Itay Cohen <itaycohen23@gmail.com>
2021-02-28 16:51:08 +0200

    Better handle project names (#2603)

--- src/core/MainWindow.cpp
+++ src/core/MainWindow.cpp
@@ -700,11 +700,16 @@ RzProjectErr MainWindow::saveProject(bool *canceled)
 
 RzProjectErr MainWindow::saveProjectAs(bool *canceled)
 {
-    QString dir = core->getConfig("prj.file");
-    if (dir.isEmpty()) {
-        dir = QDir(filename).dirName();
+    QString projectFile = core->getConfig("prj.file");
+    if (projectFile.isEmpty()) {
+        // preferred name is of fromat 'binary.exe.rzdb'
+        projectFile = QString("%1.%2").arg(filename).arg("rzdb");
     }
-    QString file = QFileDialog::getSaveFileName(this, tr("Save Project"), dir, PROJECT_FILE_FILTER);
+
+    QFileDialog fileDialog(this);
+    // Append 'rzdb' suffix if it does not exist
+    fileDialog.setDefaultSuffix("rzdb");
+    QString file = fileDialog.getSaveFileName(this, tr("Save Project"), projectFile, PROJECT_FILE_FILTER);
     if (file.isEmpty()) {
         if (canceled) {
             *canceled = true;
