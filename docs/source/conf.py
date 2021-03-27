a1f751abb36c57f623924a9648c9b5440a409730
Florian Märkl <info@florianmaerkl.de>
2021-03-27 15:58:31 +0100

    Update Version to 2.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2020, The Cutter Developers'
 author = 'The Cutter Developers'
 
 # The short X.Y version
-version = '1.12'
+version = '2.0'
 # The full version, including a2lpha/beta/rc tags
-release = '1.12.0'
+release = '2.0.0'
 
 
 # -- General configuration ---------------------------------------------------
