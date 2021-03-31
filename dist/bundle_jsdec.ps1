2c4d733b917e601d56faafa310d91f4c6fdd2ca6
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2021-03-31 20:08:58 +0300

    Use latest jsdec and rz-ghidra to match rizin version

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -2,7 +2,7 @@ $dist = $args[0]
 $python = Split-Path((Get-Command python.exe).Path)
 
 if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
-    git clone https://github.com/rizinorg/jsdec.git --branch v0.1.x --depth 1
+    git clone https://github.com/rizinorg/jsdec.git --branch v0.2.x --depth 1
 }
 cd jsdec
 & meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
