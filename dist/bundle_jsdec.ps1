78a9a112099e0d045773d77f4b1166fc3b7d2eef
Giovanni <561184+wargio@users.noreply.github.com>
2021-11-23 19:25:20 +0100

    Fix jsdec build on windows (#2840)

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -5,6 +5,12 @@ if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
     git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch v0.3.1
 }
 cd jsdec
-& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder="." -Drizin_plugdir=lib\plugins -prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
+& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." -Drizin_plugdir=lib\plugins --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
 ninja -C build install
+$ErrorActionPreference = 'Stop'
+$pathdll = "$dist\lib\plugins\core_pdd.dll"
+if(![System.IO.File]::Exists($pathdll)) {
+    type build\meson-logs\meson-log.txt
+    throw (New-Object System.IO.FileNotFoundException("File not found: $pathdll", $pathdll))
+}
 Remove-Item -Recurse -Force $dist\lib\plugins\core_pdd.lib
