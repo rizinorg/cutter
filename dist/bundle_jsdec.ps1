148e52efbc21f6cffa9c0e0061faeff21fe92295
Giovanni <561184+wargio@users.noreply.github.com>
2021-11-23 16:12:36 +0100

    Fix jsdec build on windows

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -5,6 +5,6 @@ if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
     git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch v0.3.1
 }
 cd jsdec
-& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder="." -Drizin_plugdir=lib\plugins -prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
+& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder="." -Drizin_plugdir=lib\plugins --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
 ninja -C build install
 Remove-Item -Recurse -Force $dist\lib\plugins\core_pdd.lib
