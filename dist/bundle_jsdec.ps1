71bc8eb27b0549d4bf0a075a43cd9162d1a35051
wargio <wargio@libero.it>
2021-11-23 18:28:04 +0100

    Fixed wrong dir

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -5,7 +5,7 @@ if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
     git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch v0.3.1
 }
 cd jsdec
-& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder="." -Drizin_plugdir=lib\plugins --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
+& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." -Drizin_plugdir=lib\plugins --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
 ninja -C build install
 $ErrorActionPreference = 'Stop'
 $pathdll = "$dist\lib\plugins\core_pdd.dll"
