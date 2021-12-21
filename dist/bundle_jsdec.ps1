f3fe609d171a7603e535702dd2033e60283b1fc9
Riccardo Schirone <sirmy15@gmail.com>
2021-12-21 15:52:20 +0100

    Update jsdec bundling

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -2,13 +2,13 @@ $dist = $args[0]
 $python = Split-Path((Get-Command python.exe).Path)
 
 if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
-    git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch v0.3.1
+    git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch master
 }
 cd jsdec
-& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." -Drizin_plugdir=lib\plugins --prefix=$dist --libdir=lib\plugins --datadir=lib\plugins p build
+& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." --prefix=$dist p build
 ninja -C build install
 $ErrorActionPreference = 'Stop'
-$pathdll = "$dist\lib\plugins\core_pdd.dll"
+$pathdll = "$dist\lib64\rizin\plugins\core_pdd.dll"
 if(![System.IO.File]::Exists($pathdll)) {
     type build\meson-logs\meson-log.txt
     throw (New-Object System.IO.FileNotFoundException("File not found: $pathdll", $pathdll))
