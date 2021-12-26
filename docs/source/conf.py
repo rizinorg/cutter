9e77cf1362d187699b7266b3770be8b1f6cf1100
Florian Märkl <info@florianmaerkl.de>
2021-12-26 10:07:59 +0100

    Bump version to 2.0.5 from stable (#2865)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The Cutter Developers'
 # The short X.Y version
 version = '2.0'
 # The full version, including a2lpha/beta/rc tags
-release = '2.0.4'
+release = '2.0.5'
 
 
 # -- General configuration ---------------------------------------------------
