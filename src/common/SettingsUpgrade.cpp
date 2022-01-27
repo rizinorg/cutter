bda8d994d16be9b0cfb70c2ba3e68501d9b6c8c7
billow <billow.fun@gmail.com>
2022-01-27 12:29:15 +0800

    clang-format

--- src/common/SettingsUpgrade.cpp
+++ src/common/SettingsUpgrade.cpp
@@ -9,21 +9,18 @@
  *
  * @return whether any settings have been migrated
  */
-static bool migrateSettingsPre18(QSettings& newSettings)
+static bool migrateSettingsPre18(QSettings &newSettings)
 {
-    if (newSettings.value("settings_migrated", false).toBool())
-    {
+    if (newSettings.value("settings_migrated", false).toBool()) {
         return false;
     }
     QSettings oldSettings(QSettings::NativeFormat, QSettings::Scope::UserScope, "Cutter", "Cutter");
     QStringList allKeys = oldSettings.allKeys();
-    if (allKeys.isEmpty())
-    {
+    if (allKeys.isEmpty()) {
         return false;
     }
     qInfo() << "Migrating Settings from pre-1.8";
-    for (const QString& key: allKeys)
-    {
+    for (const QString &key : allKeys) {
         newSettings.setValue(key, oldSettings.value(key));
     }
     oldSettings.clear();
@@ -45,20 +42,20 @@ static bool migrateSettingsPre18(QSettings& newSettings)
  * This function takes care of migrating from EXACTLY version X-1 to X.
  */
 
-static void migrateSettingsTo1(QSettings& settings)
+static void migrateSettingsTo1(QSettings &settings)
 {
     settings.remove("settings_migrated"); // now handled by version
     settings.remove("updated_custom_themes"); // now handled by theme_version
 }
 
-static void migrateSettingsTo2(QSettings& settings)
+static void migrateSettingsTo2(QSettings &settings)
 {
     QStringList docks = settings.value("docks").toStringList(); // get current list of docks
     // replace occurences of "PseudocodeWidget" with "DecompilerWidget"
     settings.setValue("docks", docks.replaceInStrings("PseudocodeWidget", "DecompilerWidget"));
 }
 
-static void migrateSettingsTo3(QSettings& settings)
+static void migrateSettingsTo3(QSettings &settings)
 {
     auto defaultGeometry = settings.value("geometry").toByteArray();
     auto defaultState = settings.value("state").toByteArray();
@@ -75,12 +72,10 @@ static void migrateSettingsTo3(QSettings& settings)
 #endif
 
     QVariantMap viewProperties;
-    for (auto& dock: docks)
-    {
+    for (auto &dock : docks) {
         QVariantMap properties;
         bool synchronized = true;
-        if (unsyncDocks.contains(dock))
-        {
+        if (unsyncDocks.contains(dock)) {
             synchronized = false;
         }
         properties.insert("synchronized", synchronized);
@@ -112,28 +107,27 @@ static void migrateSettingsTo3(QSettings& settings)
     settings.remove("unsync");
 }
 
-static void renameAsmOption(QSettings& settings, const QString& oldName, const QString& newName)
+static void renameAsmOption(QSettings &settings, const QString &oldName, const QString &newName)
 {
-    if (settings.contains(oldName))
-    {
+    if (settings.contains(oldName)) {
         auto value = settings.value(oldName);
         settings.remove(oldName);
         settings.setValue(newName, value);
     }
 }
 
-static void migrateSettingsTo4(QSettings& settings)
+static void migrateSettingsTo4(QSettings &settings)
 {
     renameAsmOption(settings, "asm.var.subonly", "asm.sub.varonly");
     renameAsmOption(settings, "asm.bytespace", "asm.bytes.space");
 }
 
-static void migrateSettingsTo5(QSettings& settings)
+static void migrateSettingsTo5(QSettings &settings)
 {
     renameAsmOption(settings, "asm.var.sub", "asm.sub.var");
 }
 
-static void migrateSettingsTo6(QSettings& settings)
+static void migrateSettingsTo6(QSettings &settings)
 {
     settings.remove("dir.projects");
 }
@@ -144,24 +138,17 @@ void Cutter::initializeSettings()
     QSettings settings;
 
     int settingsVersion = settings.value(CUTTER_SETTINGS_VERSION_KEY, 0).toInt();
-    if (settingsVersion == 0)
-    {
+    if (settingsVersion == 0) {
         migrateSettingsPre18(settings);
     }
 
-    if (settings.allKeys().length() > 0)
-    {
-        if (settingsVersion > CUTTER_SETTINGS_VERSION_CURRENT)
-        {
+    if (settings.allKeys().length() > 0) {
+        if (settingsVersion > CUTTER_SETTINGS_VERSION_CURRENT) {
             qWarning() << "Settings have a higher version than current! Skipping migration.";
-        }
-        else if (settingsVersion >= 0)
-        {
-            for (int v = settingsVersion + 1; v <= CUTTER_SETTINGS_VERSION_CURRENT; v++)
-            {
+        } else if (settingsVersion >= 0) {
+            for (int v = settingsVersion + 1; v <= CUTTER_SETTINGS_VERSION_CURRENT; v++) {
                 qInfo() << "Migrating Settings to Version" << v;
-                switch (v)
-                {
+                switch (v) {
                 case 1:
                     migrateSettingsTo1(settings);
                     break;
@@ -195,36 +182,31 @@ void Cutter::initializeSettings()
 static void removeObsoleteOptionsFromCustomThemes()
 {
     const QStringList options = Core()->cmdj("ecj").object().keys()
-        << ColorThemeWorker::cutterSpecificOptions;
-    RzList* themes_list = rz_core_theme_list(Core()->core());
-    RzListIter* th_iter;
-    const char* th;
-    CutterRzListForeach(themes_list, th_iter, const char, th)
-        {
-            auto theme = QString(th).trimmed();
-            if (!ThemeWorker().isCustomTheme(theme))
-            {
-                continue;
-            }
-            QJsonObject updatedTheme;
-            auto sch = ThemeWorker().getTheme(theme).object();
-            for (const auto& key: sch.keys())
-            {
-                if (options.contains(key))
-                {
-                    updatedTheme.insert(key, sch[key]);
-                }
-            }
-            ThemeWorker().save(QJsonDocument(updatedTheme), theme);
+            << ColorThemeWorker::cutterSpecificOptions;
+    RzList *themes_list = rz_core_theme_list(Core()->core());
+    RzListIter *th_iter;
+    const char *th;
+    CutterRzListForeach (themes_list, th_iter, const char, th) {
+        auto theme = QString(th).trimmed();
+        if (!ThemeWorker().isCustomTheme(theme)) {
+            continue;
         }
+        QJsonObject updatedTheme;
+        auto sch = ThemeWorker().getTheme(theme).object();
+        for (const auto &key : sch.keys()) {
+            if (options.contains(key)) {
+                updatedTheme.insert(key, sch[key]);
+            }
+        }
+        ThemeWorker().save(QJsonDocument(updatedTheme), theme);
+    }
 }
 
 void Cutter::migrateThemes()
 {
     QSettings settings;
     int themeVersion = settings.value(THEME_VERSION_KEY, 0).toInt();
-    if (themeVersion != THEME_VERSION_CURRENT)
-    {
+    if (themeVersion != THEME_VERSION_CURRENT) {
         removeObsoleteOptionsFromCustomThemes();
         settings.setValue(THEME_VERSION_KEY, THEME_VERSION_CURRENT);
     }
@@ -237,20 +219,17 @@ bool Cutter::shouldOfferSettingImport()
 {
     QSettings::setDefaultFormat(QSettings::IniFormat);
     QSettings settings;
-    if (settings.contains("firstExecution"))
-    {
+    if (settings.contains("firstExecution")) {
         return false;
     }
     QSettings r2CutterSettings(QSettings::IniFormat, QSettings::Scope::UserScope, PRE_RIZIN_ORG,
-        PRE_RIZIN_APP);
+                               PRE_RIZIN_APP);
     QString f = r2CutterSettings.fileName();
-    if (r2CutterSettings.value("firstExecution", true) != QVariant(false))
-    {
+    if (r2CutterSettings.value("firstExecution", true) != QVariant(false)) {
         return false; // no Cutter <= 1.12 settings to import
     }
     int version = r2CutterSettings.value("version", -1).toInt();
-    if (version < 1 || version > 6)
-    {
+    if (version < 1 || version > 6) {
         return false; // version too new maybe it's from r2Cutter fork instead of pre-rizin Cutter.
     }
     return true;
@@ -261,30 +240,25 @@ static void importOldSettings()
     // QSettings
     QSettings::setDefaultFormat(QSettings::IniFormat);
     QSettings r2CutterSettings(QSettings::IniFormat, QSettings::Scope::UserScope, PRE_RIZIN_ORG,
-        PRE_RIZIN_APP);
+                               PRE_RIZIN_APP);
     QSettings newSettings;
-    for (auto key: r2CutterSettings.allKeys())
-    {
+    for (auto key : r2CutterSettings.allKeys()) {
         newSettings.setValue(key, r2CutterSettings.value(key));
     }
 
     // Color Themes
-    char* szThemes = rz_str_home(".local/share/radare2/cons");
+    char *szThemes = rz_str_home(".local/share/radare2/cons");
     QString r2ThemesPath = szThemes;
     rz_mem_free(szThemes);
     QDir r2ThemesDir(r2ThemesPath);
-    if (QFileInfo(r2ThemesPath).isDir())
-    {
+    if (QFileInfo(r2ThemesPath).isDir()) {
         QDir rzThemesDir(ThemeWorker().getCustomThemesPath());
-        if (!rzThemesDir.exists())
-        {
+        if (!rzThemesDir.exists()) {
             QDir().mkpath(rzThemesDir.absolutePath());
         }
-        for (auto f: r2ThemesDir.entryInfoList(QDir::NoDotAndDotDot | QDir::Files))
-        {
+        for (auto f : r2ThemesDir.entryInfoList(QDir::NoDotAndDotDot | QDir::Files)) {
             auto dst = rzThemesDir.absoluteFilePath(f.fileName());
-            if (QDir(dst).exists())
-            {
+            if (QDir(dst).exists()) {
                 qInfo() << "Theme" << dst << "already exists. Not overwriting with"
                         << f.absoluteFilePath();
                 continue;
@@ -295,20 +269,19 @@ static void importOldSettings()
     }
 }
 
-void Cutter::showSettingImportDialog(int& argc, char** argv)
+void Cutter::showSettingImportDialog(int &argc, char **argv)
 {
     // Creating temporary QApplication because this happens before anything else in Cutter is
     // initialized
     QApplication temporaryApp(argc, argv);
     QSettings r2CutterSettings(QSettings::IniFormat, QSettings::Scope::UserScope, PRE_RIZIN_ORG,
-        PRE_RIZIN_APP);
+                               PRE_RIZIN_APP);
     QString oldFile = r2CutterSettings.fileName();
     // Can't use message translations because settings have not been imported
     auto result =
-        QMessageBox::question(nullptr, "Setting import",
-            QString("Do you want to import settings from %1?").arg(oldFile));
-    if (result == QMessageBox::Yes)
-    {
+            QMessageBox::question(nullptr, "Setting import",
+                                  QString("Do you want to import settings from %1?").arg(oldFile));
+    if (result == QMessageBox::Yes) {
         importOldSettings();
     }
 }
