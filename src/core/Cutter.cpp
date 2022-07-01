df71da8f535b82992d2166ac918072d0ac97ed80
Florian Märkl <info@florianmaerkl.de>
2022-07-01 16:49:58 +0200

    Update rizin dev (#2987)
    Relevant breaking commits in rizin:
    9ea7c2fa5acd0b9b0c178a1b4316adc129e7c512
    2987e035da827903329ec6433410899b985e2434

--- src/core/Cutter.cpp
+++ src/core/Cutter.cpp
@@ -4232,7 +4232,7 @@ QString CutterCore::getVersionInformation()
         { "rz_crypto", &rz_crypto_version },
         { "rz_bp", &rz_bp_version },
         { "rz_debug", &rz_debug_version },
-        { "rz_msg_digest", &rz_msg_digest_version },
+        { "rz_hash", &rz_hash_version },
         { "rz_io", &rz_io_version },
 #if !USE_LIB_MAGIC
         { "rz_magic", &rz_magic_version },
