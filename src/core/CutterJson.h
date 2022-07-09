e7c72c23eb499327ea82bfad8b801b9d2f3e4242
Florian Märkl <info@florianmaerkl.de>
2022-07-09 15:31:14 +0200

    Use CutterJson for Signatures and rewrite JsonModel
    This fixes a double-free in getSignatureInfo() when the string was
    already freed by the intermediate CutterJson. But actually just using
    this CutterJson makes more sense than the string anyway, so let's do
    that.
    The old JsonModel rebuilt the entire structure, defeating the main
    purpose of a custom model in comparison to the existing QTreeWidget.
    And since we are holding the json structures ourselves anyway, such a
    custom model does not have many benefits anyway.

--- src/core/CutterJson.h
+++ src/core/CutterJson.h
@@ -70,7 +70,6 @@ public:
     iterator end() const { return iterator(nullptr, nullptr); }
 
     bool toBool() const { return value && value->type == RZ_JSON_BOOLEAN && value->num.u_value; }
-    QString toJson() const { return rz_json_as_string(value); }
     st64 toSt64() const { return value && value->type == RZ_JSON_INTEGER ? value->num.s_value : 0; }
     ut64 toUt64() const { return value && value->type == RZ_JSON_INTEGER ? value->num.u_value : 0; }
 
