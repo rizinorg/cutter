254f6fb0e3dc3d3a2a4c3d6964d3095fde1ee9d8
billow <billow.fun@gmail.com>
2022-08-02 02:42:59 +0800

    fix rizin inconsistent

--- src/core/Cutter.cpp
+++ src/core/Cutter.cpp
@@ -4299,8 +4299,7 @@ void CutterCore::loadScript(const QString &scriptname)
 
 QString CutterCore::getRizinVersionReadable()
 {
-    return QString("%1 (%2)").arg(QString::fromUtf8(RZ_VERSION),
-                                  QString::fromUtf8(RZ_GITTIP).left(7));
+    return fromOwnedCharPtr(rz_str_version(nullptr));
 }
 
 QString CutterCore::getVersionInformation()
