f4be9a8266ce290bc73fd4ba114ec08606c0450c
Anton Kochkov <anton.kochkov@gmail.com>
2023-05-15 20:42:27 +0800

    Bump version to v2.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The Cutter Developers'
 # The short X.Y version
 version = '2.2'
 # The full version, including a2lpha/beta/rc tags
-release = '2.2.0'
+release = '2.2.1'
 
 
 # -- General configuration ---------------------------------------------------
