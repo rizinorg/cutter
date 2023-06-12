d597d768b759c15837ec2e3143f04d6a7deb0fb5
wargio <wargio@libero.it>
2023-06-12 23:35:33 +0800

    Fix various paths on windows

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -5,12 +5,13 @@ if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
     git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch master
 }
 cd jsdec
-& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." --prefix=$dist p build
+& meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." --prefix="$dist" p build
 ninja -C build install
 $ErrorActionPreference = 'Stop'
-$pathdll = "$dist\lib\plugins\core_pdd.dll"
+$pathdll = "$dist\lib\rizin\plugins\core_pdd.dll"
 if(![System.IO.File]::Exists($pathdll)) {
     type build\meson-logs\meson-log.txt
+    ls "$dist\lib\rizin\plugins\"
     throw (New-Object System.IO.FileNotFoundException("File not found: $pathdll", $pathdll))
 }
-Remove-Item -Recurse -Force $dist\lib\plugins\core_pdd.lib
+Remove-Item -Recurse -Force "$dist\lib\rizin\plugins\core_pdd.lib"
--- dist/bundle_rz_libyara.ps1
+++ dist/bundle_rz_libyara.ps1
@@ -10,13 +10,13 @@ if (-not (Test-Path -Path 'rz_libyara' -PathType Container)) {
 cd rz_libyara
 & meson.exe --buildtype=release --prefix=$dist build
 ninja -C build install
-$pathdll = "$dist/lib/plugins/rz_yara.dll"
+$pathdll = "$dist\lib\rizin\plugins\rz_yara.dll"
 if(![System.IO.File]::Exists($pathdll)) {
     type build/meson-logs/meson-log.txt
-    ls "$dist/lib/plugins/"
+    ls "$dist\lib\rizin\plugins\"
     throw (New-Object System.IO.FileNotFoundException("File not found: $pathdll", $pathdll))
 }
-Remove-Item -Recurse -Force $dist/lib/plugins/rz_yara.lib
+Remove-Item -Recurse -Force "$dist\lib\rizin\plugins\rz_yara.lib"
 
 cd cutter-plugin
 mkdir build
