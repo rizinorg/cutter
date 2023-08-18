4dd57f065cdcfda907b31bcf83da93024298e915
wargio <wargio@libero.it>
2023-08-18 10:13:28 +0800

    Use tags for jsdec instead of master.

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -2,7 +2,7 @@ $dist = $args[0]
 $python = Split-Path((Get-Command python.exe).Path)
 
 if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
-    git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch master
+    git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch "v0.6.0"
 }
 cd jsdec
 & meson.exe --buildtype=release -Dc_args=-DDUK_USE_DATE_NOW_WINDOWS -Djsc_folder=".." --prefix="$dist" p build
