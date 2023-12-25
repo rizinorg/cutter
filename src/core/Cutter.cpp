bff3e1b8f46d7639487cf6d9af849b94cee300ec
Anton Kochkov <anton.kochkov@gmail.com>
2023-12-25 20:19:42 +0800

    Update Rizin to the latest dev

--- src/core/Cutter.cpp
+++ src/core/Cutter.cpp
@@ -1666,7 +1666,7 @@ QVector<Chunk> CutterCore::getHeapChunks(RVA arena_addr)
             rz_list_free(arenas);
             return chunks_vector;
         }
-        m_arena = ((RzArenaListItem *)arenas->head->data)->addr;
+        m_arena = ((RzArenaListItem *)arenas->head->elem)->addr;
         rz_list_free(arenas);
     } else {
         m_arena = arena_addr;
