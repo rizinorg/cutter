70024929af52422877b1f5c63c212460b47d3a47
wargio <wargio@libero.it>
2024-02-24 23:55:19 +0800

    Build cutter native jsdec

--- scripts/jsdec.sh
+++ scripts/jsdec.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 set -e
+INSTALL_PREFIX="$1"
+EXTRA_CMAKE_OPTS="$2"
 
 SCRIPTPATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 cd "$SCRIPTPATH/.."
 
 if [ ! -d jsdec ]; then
-	git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch "v0.7.0"
+	git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch "dev"
 fi
 
 cd jsdec
-if [ -d build ]; then
-	rm -rf build
+if [ -d build_lib ]; then
+	rm -rf build_lib
 fi
-meson --buildtype=release "$@" build
-ninja -C build
-ninja -C build install
+meson setup --buildtype=release --pkg-config-path="$INSTALL_PREFIX/lib/pkgconfig" -Dbuild_type=cutter build_lib
+ninja -C build_lib
 
+mkdir build_plugin && cd build_plugin
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DJSDEC_BUILD_DIR="../build_lib" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" $EXTRA_CMAKE_OPTS ../cutter-plugin
+ninja install
