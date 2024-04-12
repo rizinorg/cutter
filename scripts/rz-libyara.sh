1c6bd79dd1fef4753d6cdf78de8545eb52f73630
wargio <wargio@libero.it>
2024-04-12 15:31:54 +0800

    disable openssl for libyara on linux/mac builds and use tagged version

--- scripts/rz-libyara.sh
+++ scripts/rz-libyara.sh
@@ -8,14 +8,14 @@ EXTRA_CMAKE_OPTS="$2"
 cd "$SCRIPTPATH/.."
 
 if [[ ! -d rz_libyara ]]; then
-	git clone https://github.com/rizinorg/rz-libyara.git --depth 1 rz_libyara
+    git clone https://github.com/rizinorg/rz-libyara.git --depth 1 --branch main rz_libyara
     git -C rz_libyara submodule init
     git -C rz_libyara submodule update
 fi
 
 cd rz_libyara
 
-meson --buildtype=release --pkg-config-path="$INSTALL_PREFIX/lib/pkgconfig" --prefix="$INSTALL_PREFIX" -Duse_sys_yara=disabled build
+meson --buildtype=release --pkg-config-path="$INSTALL_PREFIX/lib/pkgconfig" --prefix="$INSTALL_PREFIX" -Denable_openssl=false -Duse_sys_yara=disabled build
 ninja -C build install
 
 cd cutter-plugin
