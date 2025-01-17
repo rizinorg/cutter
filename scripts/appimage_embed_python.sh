e96959bdcde1eaa1b211237d9d327f25879d7cad
Karliss <karlis3p70l1ij@gmail.com>
2025-01-17 02:56:04 +0200

    More debug output.

--- scripts/appimage_embed_python.sh
+++ scripts/appimage_embed_python.sh
@@ -12,6 +12,7 @@ ls
 python_prefix=$(pkg-config --variable=prefix python3)
 
 echo "pprefix $python_prefix"
+ls /github/workspace/cutter-deps/
 ls "$python_prefix"
 ls "$python_prefix/bin"
 python_version=`$python_prefix/bin/python3 --version`
--- scripts/fetch_deps.sh
+++ scripts/fetch_deps.sh
@@ -60,6 +60,10 @@ echo "$DEPS_SHA256  $DEPS_FILE" | ${SHA256SUM} -c - || exit 1
 tar -xf "$DEPS_FILE" || exit 1
 
 if [ -f relocate.sh ]; then
+	echo "-----------before relocate"
+	cat ./python/lib/pkgconfig/*  || true
 	./relocate.sh || exit 1
+	echo "-----------after relocate"
+	cat ./python/lib/pkgconfig/*  || true
 fi
 
