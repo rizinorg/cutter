887d42c8cc60124e0890ff951f5815d3ba8d3413
Karliss <karlis3p70l1ij@gmail.com>
2025-01-17 02:22:14 +0200

    Debug python bundling problems.

--- .github/actions/build-linux-old/entrypoint.sh
+++ .github/actions/build-linux-old/entrypoint.sh
@@ -117,6 +117,7 @@ then
     locale -a
     export LANG="C.UTF-8"
     export LC_ALL="C.UTF-8"
+    echo "python prefix!!!!!!! ($CUTTER_DEPS_PYTHON_PREFIX)"
     cmake \
     -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
--- scripts/appimage_embed_python.sh
+++ scripts/appimage_embed_python.sh
@@ -7,8 +7,13 @@ if ! [[ $# -eq 2 ]]; then
     exit 1
 fi
 
+pwd
+ls
 python_prefix=$(pkg-config --variable=prefix python3)
 
+echo "pprefix $python_prefix"
+ls "$python_prefix"
+ls "$python_prefix/bin"
 python_version=`$python_prefix/bin/python3 --version`
 python_version=${python_version##* }
 python_version=python${python_version%.*}
