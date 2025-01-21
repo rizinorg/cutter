bef6d6b042de54bdec1cb8bfbeb424a7c7876d05
Karliss <karlis3p70l1ij@gmail.com>
2025-01-21 19:02:56 +0200

    Format.

--- src/menus/DisassemblyContextMenu.cpp
+++ src/menus/DisassemblyContextMenu.cpp
@@ -1097,7 +1097,7 @@ void DisassemblyContextMenu::on_actionEditFunction_triggered()
                 fcn->cc = rz_str_constpool_get(&core->analysis->constpool, newCC.constData());
             }
 
-            emit Core() -> functionsChanged();
+            emit Core()->functionsChanged();
         }
     }
 }
