7379e6c111148d057b27a077b57f4f625200ad2a
Anohkka <61073541+Anohkka@users.noreply.github.com>
2025-04-08 12:42:45 -0300

    Fallback formula is once again used only for large files
    Was using the fallback formula for small files while testing and forgot to change it back before committing

--- src/widgets/DisassemblyWidget.cpp
+++ src/widgets/DisassemblyWidget.cpp
@@ -792,9 +792,9 @@ RVA DisassemblyScrollArea::currentVScrollAddr()
         return beginOffset;
     }
     // Fallback formula for large files
-    if ((RVA_MAX / maximum) > binSize()) {
+    if ((RVA_MAX / maximum) < binSize()) {
         return verticalScrollBar()->value() * (binSize() / maximum)
-                + std::min(static_cast<RVA>(verticalScrollBar()->value()), binSize() % maximum)
+                + std::min<RVA>(verticalScrollBar()->value(), binSize() % maximum)
                 + beginOffset;
     }
     return (verticalScrollBar()->value() * binSize()) / maximum + beginOffset;
@@ -813,7 +813,7 @@ void DisassemblyScrollArea::setVScrollPos(RVA address)
         verticalScrollBar()->setValue(scrollBarPos);
         return;
     }
-    if ((RVA_MAX / maximum) > binSize()) {
+    if ((RVA_MAX / maximum) < binSize()) {
         if (address > endOffset) {
             // Fallback formula can handle numbers within the binary's range
             // but not numbers far above it
