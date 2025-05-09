0f957970f5746abcd69a1e87484cd6ab4dcd687a
Karliss <karlis3p70l1ij@gmail.com>
2025-05-09 20:50:03 +0300

    Bump cutter version to 2.4.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The Cutter Developers'
 # The short X.Y version
 version = '2.4'
 # The full version, including a2lpha/beta/rc tags
-release = '2.4.0'
+release = '2.4.1'
 
 
 # -- General configuration ---------------------------------------------------
