505cb45938a3020f9e211c54ccf6348d0eea5b8c
PremadeS <128969155+PremadeS@users.noreply.github.com>
2025-06-30 02:21:10 +0500

    Fix: incomplete type `QAction`

--- src/shortcuts/ShortcutManager.cpp
+++ src/shortcuts/ShortcutManager.cpp
@@ -70,9 +70,9 @@ void ShortcutManager::setupAction(QAction &action, const QString &id)
 QShortcut *ShortcutManager::makeQShortcut(const QString &id, QObject *parent)
 {
     QShortcut *shortcut = new QShortcut(parent);
-    QKeySequence keySequence = getKeySequence(id);
-    if (!keySequence.isEmpty()) {
-        shortcut->setKey(keySequence);
+    QList<QKeySequence> keySequences = getKeySequences(id);
+    if (!keySequences.isEmpty()) {
+        shortcut->setKeys(keySequences);
     }
     return shortcut;
 }
--- src/shortcuts/ShortcutManager.h
+++ src/shortcuts/ShortcutManager.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QWidget>
 #include <QKeySequence>
+#include <QAction>
 #include <QShortcut>
 #include "DefaultShortcuts.h"
 
