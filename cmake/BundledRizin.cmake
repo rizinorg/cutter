15928ca2d0b1a619c1a188e51862a23799057e8b
PremadeS <128969155+PremadeS@users.noreply.github.com>
2025-09-03 23:57:56 +0500

    Remove: use_swift_demangler in `BundledRizin.cmake`

--- cmake/BundledRizin.cmake
+++ cmake/BundledRizin.cmake
@@ -22,10 +22,6 @@ if (CUTTER_ENABLE_SIGDB)
     list(APPEND MESON_OPTIONS "-Dinstall_sigdb=true")
 endif()
 
-if (CUTTER_PACKAGE_RZ_LIBSWIFT AND CUTTER_ENABLE_DEPENDENCY_DOWNLOADS)
-    list(APPEND MESON_OPTIONS "-Duse_swift_demangler=false")
-endif()
-
 find_program(MESON meson)
 if(NOT MESON)
     message(FATAL_ERROR "Failed to find meson, which is required to build bundled rizin")
