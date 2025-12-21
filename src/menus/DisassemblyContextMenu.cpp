e738f69517d2102cdb856fc66ac82acc7daccfc5
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2025-12-21 13:35:30 +0800

    Fix Cutter build due to rizinorg/rizin#5613

--- src/menus/DisassemblyContextMenu.cpp
+++ src/menus/DisassemblyContextMenu.cpp
@@ -515,8 +515,8 @@ void DisassemblyContextMenu::aboutToShowSlot()
     st64 memDisp = 0; // Displacement
 
     if (ab && ab->op) {
-        const char *opexstr = RZ_STRBUF_SAFEGET(&ab->op->opex);
-        CutterJson operands = Core()->parseJson("opex", strdup(opexstr), nullptr);
+        CutterJson operands = Core()->parseJson("opex", rz_structured_data_to_json(ab->op->opex),
+                                                nullptr);
 
         // Loop through both the operands of the instruction
         for (const CutterJson operand : operands) {
