d02c98355e250affb3c0d6ac40ff1a3f2663c746
Anton Kochkov <anton.kochkov@gmail.com>
2026-06-21 14:42:22 +0400

    Use rz-libyara v0.9.0

--- scripts/rz-libyara.sh
+++ scripts/rz-libyara.sh
@@ -8,7 +8,7 @@ EXTRA_CMAKE_OPTS="$2"
 cd "$SCRIPTPATH/.."
 
 if [[ ! -d rz_libyara ]]; then
-    git clone https://github.com/rizinorg/rz-libyara.git --depth 1 --branch main rz_libyara
+    git clone https://github.com/rizinorg/rz-libyara.git --depth 1 --branch "v0.9.0" rz_libyara
     git -C rz_libyara submodule init
     git -C rz_libyara submodule update
 fi
