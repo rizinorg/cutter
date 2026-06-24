rizinorg/cutter

commit 4e53fd64d36274460e60623108b58c0d397033aa

Update rizin to latest dev. (#3514)
* Update rizin to latest stable.
* Remove: use_swift_demangler in `BundledRizin.cmake`
* Remove: `rz_bp` in `BundledRizin.cmake`
* Fix: use sys_path
* Bump `Rizin_VERSION` to 0.9

src/common/ColorThemeWorker.cpp
@@ -37,7 +37,8 @@ ColorThemeWorker::ColorThemeWorker(QObject *parent) : QObject(parent)
         QDir().mkpath(customRzThemesLocationPath);
     }
 
-    char *theme_dir = rz_path_prefix(RZ_THEMES);
+    RzPath *sys_path = rz_path_new();
+    const char *theme_dir = rz_path_system(sys_path, RZ_THEMES);
     QDir currDir { theme_dir };
     if (currDir.exists()) {
         standardRzThemesLocationPath = currDir.absolutePath();
@@ -47,7 +48,7 @@ ColorThemeWorker::ColorThemeWorker(QObject *parent) : QObject(parent)
                                  "Most likely, Rizin is not properly installed.")
                                       .arg(currDir.path()));
     }
-    free(theme_dir);
+    rz_path_free(sys_path);
 }
 
 QColor ColorThemeWorker::mergeColors(const QColor &upper, const QColor &lower) const

cmake/BundledRizin.cmake
@@ -22,10 +22,6 @@ if (CUTTER_ENABLE_SIGDB)
     list(APPEND MESON_OPTIONS "-Dinstall_sigdb=true")
 endif()
 
-if (CUTTER_PACKAGE_RZ_LIBSWIFT AND CUTTER_ENABLE_DEPENDENCY_DOWNLOADS)
-    list(APPEND MESON_OPTIONS "-Duse_swift_demangler=false")
-endif()
-
 find_program(MESON meson)
 if(NOT MESON)
     message(FATAL_ERROR "Failed to find meson, which is required to build bundled rizin")
@@ -57,10 +53,10 @@ endif()
 
 # TODO: This version number should be fetched automatically
 # instead of being hardcoded.
-set (Rizin_VERSION 0.8)
+set (Rizin_VERSION 0.9)
 
 set (RZ_LIBS rz_core rz_config rz_cons rz_io rz_util rz_flag rz_arch rz_debug
-        rz_hash rz_bin rz_lang rz_il rz_bp rz_egg rz_reg rz_search rz_syscall
+        rz_hash rz_bin rz_lang rz_il rz_egg rz_reg rz_search rz_syscall
         rz_socket rz_magic rz_crypto rz_type rz_diff rz_sign rz_demangler)
 set (RZ_EXTRA_LIBS rz_main)
 set (RZ_BIN rz-bin rizin rz-diff rz-find rz-gg rz-hash rz-run rz-asm rz-ax rz-sign)

src/core/Cutter.cpp
@@ -192,17 +192,18 @@ CutterCore *CutterCore::instance()
 
 void CutterCore::initialize(bool loadPlugins)
 {
+    rz_cons_new(); // initialize console
+    core_ = rz_core_new();
+
 #if defined(MACOS_RZ_BUNDLED)
     auto app_path = QDir(QCoreApplication::applicationDirPath());
     app_path.cdUp();
     app_path.cd("Resources");
     qInfo() << "Setting Rizin prefix =" << app_path.absolutePath()
             << " for macOS Application Bundle.";
-    rz_path_set_prefix(app_path.absolutePath().toUtf8().constData());
+    rz_path_set_prefix(core_->sys_path, app_path.absolutePath().toUtf8().constData());
 #endif
 
-    rz_cons_new(); // initialize console
-    core_ = rz_core_new();
     char **env = rz_sys_get_environ();
     core_->io->envprofile = rz_run_get_environ_profile(env);
     rz_core_task_sync_begin(&core_->tasks);
@@ -4628,7 +4629,7 @@ void CutterCore::loadScript(const QString &scriptname)
 
 QString CutterCore::getRizinVersionReadable(const char *program)
 {
-    return fromOwnedCharPtr(rz_version_str(program));
+    return fromOwnedCharPtr(rz_version_str(core_->sys_path, program));
 }
 
 QString CutterCore::getVersionInformation()
@@ -4648,7 +4649,6 @@ QString CutterCore::getVersionInformation()
         { "rz_flag", &rz_flag_version },
         { "rz_core", &rz_core_version },
         { "rz_crypto", &rz_crypto_version },
-        { "rz_bp", &rz_bp_version },
         { "rz_debug", &rz_debug_version },
         { "rz_hash", &rz_hash_version },
         { "rz_io", &rz_io_version },

commit ae8a3fbb519bfcbf0ae1f90fbf53640e7c0d0eaf

Fix portable rizin system themes loading
Regression introduced in 4e53fd64 for the macOS application bundle:
rz_path_set_prefix() is only applied to the core-global RzPath instance,
but the ColorThemeWorker created its own instance, ignoring this
setting.
This led to the message box saying "The Rizin standard themes could not
be found in..." on startup.

src/common/ColorThemeWorker.cpp
@@ -56,8 +56,8 @@ ColorThemeWorker::ColorThemeWorker(QObject *parent) : QObject(parent)
         QDir().mkpath(customRzThemesLocationPath);
     }
 
-    RzPath *sysPath = rz_path_new();
-    const char *themeDir = rz_path_system(sysPath, RZ_THEMES);
+    RzCoreLocked core = Core()->lock();
+    const char *themeDir = rz_path_system(core->sys_path, RZ_THEMES);
     const QDir currDir { themeDir };
     if (currDir.exists()) {
         standardRzThemesLocationPath = currDir.absolutePath();
@@ -67,7 +67,6 @@ ColorThemeWorker::ColorThemeWorker(QObject *parent) : QObject(parent)
                                  "Most likely, Rizin is not properly installed.")
                                       .arg(currDir.path()));
     }
-    rz_path_free(sysPath);
 }
 
 QColor ColorThemeWorker::mergeColors(const QColor &upper, const QColor &lower) const