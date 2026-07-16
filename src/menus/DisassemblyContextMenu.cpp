0d8b40968c09f19cc2360741b2043ce1a423ee7a
andersendsa <andersendsa631@gmail.com>
2026-07-16 16:04:43 +0530

    format fix

--- src/menus/DisassemblyContextMenu.cpp
+++ src/menus/DisassemblyContextMenu.cpp
@@ -1042,7 +1042,7 @@ void DisassemblyContextMenu::editFunctionTriggered()
                 rz_analysis_function_set_cc(core->analysis, fcn, newCC.constData());
             }
 
-            emit Core() -> functionsChanged();
+            emit Core()->functionsChanged();
         }
     }
 }
